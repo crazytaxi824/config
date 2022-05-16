f0efda7624010229af78530a7386464410518229
crazytaxi <crazytaxi.git@outlook.com>
2022-05-16 20:55:01 +1000

    up jump_to_log

--- nvim/after/syntax/go/jump_to_log.lua
+++ nvim/after/syntax/go/jump_to_log.lua
@@ -1,44 +1,58 @@
 -- 利用 local list 跳转到 log 文件 -----------------------------------------------------------------
--- 用来储存 setloclist() 用的 winnr.
--- FIXME WinEnter 时更新 go_run_win_id
-go_run_win_id = vim.fn.win_getid()
--- if not go_run_win_id then
---   print('not')
---   go_run_win_id = {vim.fn.win_getid()}
--- else
---   print('ok')
---   table.insert(go_run_win_id, vim.fn.win_getid())
--- end
 
 -- 操作方法: cursor 需要在 filepath string 上, 然后使用 <CR> 跳转到文件.
 local function gotoLogFile()
   local lcontent = vim.fn.expand('<cWORD>')  -- <cWORD> 以 %s 切分.
-
   local fp = vim.split(lcontent, ":")
 
   local lnum = tonumber(fp[2])
-  if not lnum then
+  if not lnum then  -- 如果没有 lnum 则设为 1
     lnum = 1
   end
 
-  local locitem = nil
-  if vim.fn.filereadable(fp[1]) == 1 then
-    locitem = {filename = fp[1], lnum = lnum}
-  else
-    -- 如果 go_run_win_id 窗口中的 bufname 和 log 打印出来的文件名相同, 则跳转到该文件.
-    local bufname = vim.fn.bufname(vim.fn.getwininfo(go_run_win_id)[1].bufnr)
-    if vim.fn.fnamemodify(bufname, ':t') == fp[1] then
-      locitem = {filename = bufname, lnum = lnum}
+  local go_run_win_id = -1  -- 用于设置 setloclist()
+  local loc_items = nil     -- 初始化 local list item
+
+  -- 寻找和 log 打印的 filepath 相同的 win_id. 如果有则跳转到该 window.
+  for _, win_info in ipairs(vim.fn.getwininfo()) do
+    local bufpath = vim.fn.fnamemodify(vim.fn.bufname(win_info.bufnr), ':p')  -- bufname full path
+
+    -- FIXME when `go test` could print absolute path
+    -- https://github.com/golang/vscode-go/issues/1875
+    -- https://github.com/golang/go/issues/47399
+    if string.match(bufpath, fp[1]) then  -- NOTE: 这里是临时措施, 主要是为 t.Log() 打印路径.
+    --local logpath = vim.fn.fnamemodify(fp[1], ':p')  -- log full path
+    --if bufpath == logpath then  -- 如果 bufpath 绝对路径和 logpath 绝对路径完全相同的情况下.
+      go_run_win_id = win_info.winid
+      loc_items = {filename = bufpath, lnum = lnum}
+      break
+    elseif string.match(bufpath, ".*%.go$") then
+      -- 如果所有的 win 中都没有 log 打印的 filepath, 则选择一个 .go 文件的 winid 用于跳转.
+      go_run_win_id = win_info.winid
     end
   end
 
-  if locitem then
-    -- TODO if go_run_win_id 不存在, 则创建一个新的 window.
+  -- 如果所有的 win 中都没有 log 打印的 filepath, 则检查该 <cWORD> 是否是可以打开的文件.
+  if not loc_items then
+    if vim.fn.filereadable(fp[1]) == 1 then
+      loc_items = {filename = fp[1], lnum = lnum}
+    end
+  end
 
-    vim.fn.setloclist(go_run_win_id, {locitem}, 'r')  -- 给指定 window 设置 loclist
-    vim.fn.win_gotoid(go_run_win_id)  -- 必须在该 window 下才能打开 loclist
-    vim.cmd('lfirst')  -- jump to loclist first item
-    vim.fn.setloclist(go_run_win_id, {}, 'r')  -- clear loclist
+  -- 如果有 local list item, 则进行跳转.
+  if loc_items then
+    if vim.fn.win_gotoid(go_run_win_id) == 1 then
+      vim.fn.setloclist(go_run_win_id, {loc_items}, 'r')  -- 给指定 window 设置 loclist
+      vim.cmd('lfirst')  -- jump to loclist first item
+      vim.fn.setloclist(go_run_win_id, {}, 'r')  -- clear loclist
+    else
+      -- 如果 go_run_win_id 不存在, 则创建一个新的 window.
+      vim.cmd('botright vsplit ' .. loc_items.filename)
+      go_run_win_id = vim.fn.win_getid()
+      vim.fn.setloclist(go_run_win_id, {loc_items}, 'r')  -- 给指定 window 设置 loclist
+      vim.cmd('lfirst')  -- jump to loclist first item
+      vim.fn.setloclist(go_run_win_id, {}, 'r')  -- clear loclist
+    end
   end
 end
 
