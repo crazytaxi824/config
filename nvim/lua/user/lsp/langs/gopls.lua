e4b8e23458515c682b4061466e27aa6c4a04ba09
crazytaxi <crazytaxi.git@outlook.com>
2022-05-31 19:42:04 +1000

    update gopls root_dir ignore workspace folders

--- nvim/lua/user/lsp/langs/gopls.lua
+++ nvim/lua/user/lsp/langs/gopls.lua
@@ -2,6 +2,9 @@
 -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
 -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-config
 
+--- NOTE: ignore following folds as workspace root directory.
+local ignore_workspace_folders = { "$HOME", "$GOPATH/pkg/mod/github.com/hashicorp/consul/api@v1.12.0" }
+
 return {
   --cmd = { "gopls" },
   --filetypes = {"go", "gomod"},
@@ -9,10 +12,17 @@ return {
   --- NOTE: lspconfig.util.root_pattern() 只能在这里使用, 不能在 project_lsp_config 中使用.
   --- project_lsp_config 中设置 root_dir 直接使用 string. eg: root_dir = "/a/b/c"; root_dir = vim.fn.getcwd().
   --- 以下是默认设置. https://github.com/neovim/nvim-lspconfig -> lua/lspconfig/server_configurations/gopls.lua
-  --root_dir = function(fname)  -- fname == :echo expand('%:p')
-  --  local util = require("lspconfig/util")
-  --  return util.root_pattern 'go.work'(fname) or util.root_pattern('go.mod', '.git')(fname)
-  --end,
+  root_dir = function(fname)  -- fname == :echo expand('%:p') 当前文件绝对路径.
+    --- ignore workspace folders
+    for _, ignored in ipairs(ignore_workspace_folders) do
+      if string.match(fname, vim.fn.expand(ignored)) then
+        return vim.fn.getcwd()  -- 返回当前 current working directory = pwd
+      end
+    end
+
+    local util = require("lspconfig/util")
+    return util.root_pattern 'go.work'(fname) or util.root_pattern('go.mod', '.git')(fname)
+  end,
 
   settings = {
     gopls = {
