e1a6f9f94c46c4420ff9100f7b1884e498e8d1c8
crazytaxi <crazytaxi.git@outlook.com>
2022-06-04 16:52:40 +1000

    up

--- nvim/lua/user/settings.lua
+++ nvim/lua/user/settings.lua
@@ -224,11 +224,5 @@ vim.cmd [[au Filetype qf :wincmd J]]
 -- markdown 文件自动执行 SpellCheck 命令
 --vim.cmd [[au Filetype pandoc,markdown setlocal spell spelllang=en,cjk]]
 
---- test function ----------------------------------------------------------------------------------
---- NOTE: after enabling 'exrc', neovim will execute any '.nvimrc' or '.exrc' at project's root directory.
---- https://github.com/neovim/nvim-lspconfig/wiki/Project-local-settings
---vim.opt.exrc = true
-
-
 
 
