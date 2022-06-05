5ada7607a42f9c8a3567ef75f20db6825343efd6
crazytaxi <crazytaxi.git@outlook.com>
2022-06-05 17:24:43 +1000

    up indentline

--- nvim/lua/user/plugin-settings/indentline.lua
+++ nvim/lua/user/plugin-settings/indentline.lua
@@ -6,7 +6,7 @@ if not status_ok then
   return
 end
 
-vim.g.indent_blankline_use_treesitter = true  -- NOTE: use treesitter
+-- vim.g.indent_blankline_use_treesitter = "v:true"  -- NOTE: use treesitter
 vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
 vim.g.indent_blankline_filetype_exclude = {
   "help",
@@ -18,9 +18,7 @@ vim.g.indent_blankline_filetype_exclude = {
   "Trouble",
 }
 vim.g.indentLine_enabled = 1
-vim.g.indent_blankline_char = "│"
---vim.g.indent_blankline_char = "▏"
---vim.g.indent_blankline_char = "▎"
+vim.g.indent_blankline_char = "│"  -- 默认 "▏" and "▎"
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = true
 vim.g.indent_blankline_show_current_context = true
