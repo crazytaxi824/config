c4abc23192d0cd21a4ce74f6be83ee4210f34e4e
crazytaxi <crazytaxi.git@outlook.com>
2022-06-27 00:03:16 +1000

    update indent_line setup()

--- nvim/lua/user/plugin-settings/indentline.lua
+++ nvim/lua/user/plugin-settings/indentline.lua
@@ -6,58 +6,39 @@ if not status_ok then
   return
 end
 
--- vim.g.indent_blankline_use_treesitter = "v:true"  -- NOTE: use treesitter, 默认 v:false.
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_filetype_exclude = {
-  "help",
-  "startify",
-  "dashboard",
-  "packer",
-  "neogitstatus",
-  "NvimTree",
-  "Trouble",
-}
-vim.g.indentLine_enabled = 1
-vim.g.indent_blankline_char = "│"  -- 默认 "▏" and "▎"
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_first_indent_level = true
-vim.g.indent_blankline_show_current_context = true
-
---- indent_blankline_context_patterns -------------------------------------------------------------- {{{
---vim.g.indent_blankline_context_patterns = {
---  "class",
---  "return",
---  "function",
---  "method",
---  "^if",
---  "^while",
---  "jsx_element",
---  "^for",
---  "^object",
---  "^table",
---  "block",
---  "arguments",
---  "if_statement",
---  "else_clause",
---  "jsx_element",
---  "jsx_self_closing_element",
---  "try_statement",
---  "catch_clause",
---  "import_statement",
---  "operation_type",
---}
--- -- }}}
-
 --- set list & set listchars, NOTE: 在 settings.lua 中设置.
 --vim.opt.list = true
 --vim.opt.listchars:append("space:⋅")
 --vim.opt.listchars:append("eol:↴")
 
 indent_blankline.setup({
+  enabled = true,
+  use_treesitter = true,  -- NOTE: use treesitter, 默认 false.
+  --use_treesitter_scope = true,  -- Instead of using |g:indent_blankline_context_patterns|, NOTE: 还不是很完善.
+
+  buftype_exclude = { "terminal", "nofile", "quickfix" },
+  bufname_exclude = {'README.md', '.*\\.py'},  -- python 不适合 indent line.
+  filetype_exclude = {
+    "help",
+    "startify",
+    "dashboard",
+    "packer",
+    "neogitstatus",
+    "NvimTree",
+    "Trouble",
+  },
+
+  char = "│",  -- 默认 "▏" and "▎"
+
+  show_trailing_blankline_indent = false,  -- VVI: 在 ) } 后不再显示 indent char.
+  show_first_indent_level = true,
+
+  --show_current_context = true,  -- 显示 indentLine 颜色. NOTE: 需要安装 nvim-treesitter. autocmd CursorMoved 影响速度.
+  --show_current_context_start = true,  -- 在 indentLine 起始行添加下划线.
+  --show_current_context_start_on_current_line = true,  -- even when the cursor is on the same line.
+
   --show_end_of_line = true,
   --space_char_blankline = " ",
-  show_current_context = false,  -- 默认 true 显示 indentLine 颜色; 不建议开启, 有 BUG
-  --show_current_context_start = true,
 
   --- NOTE: 加载下面定义的颜色设置 --- {{{
   --char_highlight_list = {
