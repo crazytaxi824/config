3672ceab8f8cccb676d0551d89f1a3f4ab5d47c4
crazytaxi <crazytaxi.git@outlook.com>
2022-07-12 23:07:18 +1000

    up

--- nvim/lua/user/plugin_settings/lualine.lua
+++ nvim/lua/user/plugin_settings/lualine.lua
@@ -38,7 +38,6 @@ local my_theme = {
 lualine.setup {
   options = {
     theme = my_theme,  -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
-    -- theme = "ayu_mirage",  -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
     icons_enabled = false, -- 不使用 icon, NOTE: 可以在 sections 中单独设置. `:help lualine-Global-options`
     component_separators = { left = '', right = ''},
     section_separators = { left = ' ', right = ' '},
@@ -64,6 +63,7 @@ lualine.setup {
           readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
           unnamed = '[No Name]', -- Text to show for unnamed buffers.
         },
+        --- NOTE: output format
         -- fmt = function(content)
         --   print(content:sub(-3,-1))
         --   return content
@@ -84,14 +84,10 @@ lualine.setup {
     },
   },
 
+  --- cursor 不在窗口时(失去焦点的窗口)所显示的信息, 以及颜色.
   inactive_sections = {
-    lualine_a = {'mode'},
-    lualine_b = {
-      {'branch',
-        icons_enabled = true, -- 单独设置 branch 使用 icon.
-        icon = {'', color={fg='green'}},
-      }
-    },
+    lualine_a = {},
+    lualine_b = {},
     lualine_c = {
       {'filename',
         path = 3,  -- Absolute path, with ~ as the home directory
@@ -100,43 +96,12 @@ lualine.setup {
           readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
           unnamed = '[No Name]', -- Text to show for unnamed buffers.
         },
-        -- fmt = function(content)
-        --   print(content:sub(-3,-1))
-        --   return content
-        -- end,
       },
     },
-    lualine_x = {'encoding', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location',
-      {'diagnostics',
-        diagnostics_color = {
-          error = 'ErrorMsg',        -- Changes diagnostics' error color.
-          warn  = 'WarningMsg',      -- Changes diagnostics' warn color.
-          info  = 'SpecialComment',  -- Changes diagnostics' info color.
-          hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
-        },
-      },
-    },
+    lualine_x = {'diagnostics'},
+    lualine_y = {},
+    lualine_z = {}
   },
-  --- cursor 不在窗口时(失去焦点的窗口)所显示的信息, 以及颜色.
-  -- inactive_sections = {
-  --   lualine_a = {},
-  --   lualine_b = {},
-  --   lualine_c = {
-  --     {'filename',
-  --       path = 3,  -- Absolute path, with ~ as the home directory
-  --       symbols = {
-  --         modified = '[+]',      -- Text to show when the file is modified.
-  --         readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
-  --         unnamed = '[No Name]', -- Text to show for unnamed buffers.
-  --       },
-  --     },
-  --   },
-  --   lualine_x = {'diagnostics'},
-  --   lualine_y = {},
-  --   lualine_z = {}
-  -- },
 
   --- display components in tabline at top.
   tabline = {},
