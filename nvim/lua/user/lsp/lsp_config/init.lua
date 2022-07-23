56777490e5d22b7dc5a8f320bf97a2de9fc1d0e1
crazytaxi <crazytaxi.git@outlook.com>
2022-07-24 00:12:25 +1000

    update: lspconfig

--- nvim/lua/user/lsp/lsp_config/init.lua
+++ nvim/lua/user/lsp/lsp_config/init.lua
@@ -10,35 +10,16 @@ if not lsp_installer_ok then
   return
 end
 
---- VVI: 需要启用的 LSP server --------------------------------------------------------------------- {{{
---- 这里的 lsp server 都是通过 lsp-installer 安装的, 所以使用其提供的函数获取已安装列表.
---- get_available_servers()  -- 所有可以安装的 lsp server.
---- get_installed_servers()  -- 所有已安装的 lsp server.
-
---- 获取所有通过 lsp-installer 安装的 lsp server.
---- NOTE: 第一次打开文件的时候 lsp_installer.get_installed_servers() 无法找到 gopls. 不知道为什么, 暂时不使用该方法.
--- local function get_installed_server_names()
---   local svr_list = {}
---   for _, svr in ipairs(lsp_installer.get_installed_servers()) do
---     print(svr.name)
---     table.insert(svr_list, svr.name)  -- table.insert() 没有返回值.
---   end
---   return svr_list
--- end
---
--- local LSP_servers = get_installed_server_names()
--- print(vim.inspect(LSP_servers))
--- -- }}}
 --- 手动指定需要启用的 lsp server.
 --- 如果 lsp 已安装, 但是不在列表中也不会启动. 因为 lspconfig[LSP_server].setup() 也需要用到该 list.
-local LSP_servers = { "jsonls", "sumneko_lua", "gopls", "tsserver", "pyright", "html", "cssls", "bashls" }
+local lsp_servers = { "jsonls", "sumneko_lua", "gopls", "tsserver", "pyright", "html", "cssls", "bashls" }
 
 --- nvim-lsp-installer setup() ---------------------------------------------------------------------
 lsp_installer.setup {
   -- NOTE: 其实这里不需要通过 lsp-installer 安装 gopls, 因为 gopls 在 $PATH 中,
   -- 所以 lspconfig 可以直接使用 gopls;
   -- 如果 gopls 不在 $PATH 中, 则使用 lsp-installer 下载的 gopls.
-  ensure_installed = LSP_servers,   -- list 中设置的 LSP 如果没有安装, 则自动安装.
+  ensure_installed = lsp_servers,   -- list 中设置的 LSP 如果没有安装, 则自动安装.
 
   -- NOTE: LSP server 下载位置默认在 "~/.local/share/nvim/lsp_servers/..."
   --install_root_dir = vim.fn.stdpath("data") .. "/lsp_servers",
@@ -68,23 +49,23 @@ lsp_installer.setup {
 }
 
 --- lspconfig setup() ------------------------------------------------------------------------------
-for _, LSP_server in pairs(LSP_servers) do
+for _, lsp_svr in pairs(lsp_servers) do
   --- NOTE: opts 必须包含 on_attach, capabilities 两个属性.
   ---       这里的 opts 获取到的是 require 文件中返回的 M.
   local opts = require("user.lsp.lsp_config.setup_opts")
 
   --- NOTE: 加载 lsp 配置文件, "~/.config/nvim/lua/user/lsp/lsp_config/langs/..."
   --- 如果文件存在, 则加载自定义设置, 如果没有自定义设置则加载默认设置.
-  if vim.fn.filereadable(vim.fn.stdpath('config') .. '/lua/user/lsp/lsp_config/langs/' .. LSP_server .. '.lua') == 1 then
+  if vim.fn.filereadable(vim.fn.stdpath('config') .. '/lua/user/lsp/lsp_config/langs/' .. lsp_svr .. '.lua') == 1 then
     --- NOTE: 这里使用 pcall() 是为了确保 xxx.lua 文件执行没有问题.
-    local lsp_custom_status_ok, lsp_custom_opts = pcall(require, "user.lsp.lsp_config.langs." .. LSP_server)
+    local lsp_custom_status_ok, lsp_custom_opts = pcall(require, "user.lsp.lsp_config.langs." .. lsp_svr)
     if lsp_custom_status_ok then
       opts = vim.tbl_deep_extend("force", opts, lsp_custom_opts)
     end
   end
 
   --- VVI: 这里就是 lspconfig.xxx.setup() 针对不同的 lsp 进行加载.
-  lspconfig[LSP_server].setup(opts)
+  lspconfig[lsp_svr].setup(opts)
 end
 
 --- 其他 LSP 相关设置 ------------------------------------------------------------------------------
--- nvim/lua/user/lsp/lsp_config/setup_opts.lua
+++ nvim/lua/user/lsp/lsp_config/setup_opts.lua
@@ -90,6 +90,9 @@ M.on_init = function(client)
   return true  -- VVI: 如果 return false 则 LSP 不启动.
 end
 
+--- 可以手动通过 :LspStart gopls 启动 lsp
+--M.autostart = false
+
 return M
 
 
