e61f1c9f62aa717abad089628236214ba768b329
crazytaxi <crazytaxi.git@outlook.com>
2022-07-24 17:37:39 +1000

    fix: hover_short() for generics

--- nvim/lua/user/lsp/lsp_config/user_lsp_request.lua
+++ nvim/lua/user/lsp/lsp_config/user_lsp_request.lua
@@ -1,3 +1,31 @@
+--- NOTE: 这里 node_row 和 node_char 都是从 0 开始计算.
+--- node_char 是指行内第几个字符, \t 算一个字符.
+local function calculate_offset(lsp_req_pos_line, lsp_req_pos_char)
+  local cursor_pos = vim.fn.getpos('.')  -- 光标位置, 返回 [bufnum, lnum, col, off]. lnum 和 col 都是从 1 开始计算.
+
+  --- VVI: offset 中 \t 占4个字符位置, 而在 getpos() 和 node:start() 中只占1个字符.
+  --- strdisplaywidth() 会计算实际显示宽度, \t 会被计算在显示宽度之内.
+  --- node 行 virtual column 位置.
+  local node_display_width = vim.fn.strdisplaywidth(string.sub(vim.fn.getline(lsp_req_pos_line+1), 0, lsp_req_pos_char))
+  --- cursor 行 virtual column 位置.
+  local cursor_display_width = vim.fn.strdisplaywidth(string.sub(vim.fn.getline(cursor_pos[2]), 0, cursor_pos[3]-1))
+
+  --- 计算 open_floating_preview() 横向/纵向偏移量
+  local offset_x = node_display_width - cursor_display_width
+  local offset_y = lsp_req_pos_line - cursor_pos[2] +1
+
+  --- VVI:
+  --- offsetX - float_window 距离 cursor 的位置偏移.
+  --- offsetY - 同上, 由于 getpos() 返回的 lnum 是从 1 开始, 而 node:start() 返回的 row 是从 0 开始,
+  ---           所以 offsetY 的结果需要 +1.
+  return {
+    lsp_req_pos_line = lsp_req_pos_line,
+    lsp_req_pos_char = lsp_req_pos_char,
+    offset_x = offset_x,
+    offset_y = offset_y,
+  }
+end
+
 --- NOTE: 使用 nvim-treesitter 寻找 cursor 前最近的 function call() 的位置 --- {{{
 -- `:help nvim-treesitter`
 --    node = ts_utils.get_node_at_cursor()  -- 获取 node at cursor.
@@ -5,7 +33,11 @@
 -- `:help treesitter`
 --    node:start()  -- start pos, return [row, (col), totalbytes]
 --    node:end_()   -- end pos
---    node:parent() -- 父级 node
+--    node:parent() -- 父级 node, 查找 arguments
+--    node:prev_sibling()  -- 查找 function
+--    node:next_sibling()
+--    node:prev_named_sibling()  -- 查找 function
+--    node:next_named_sibling()
 --    node:type()   -- treesitter 分析
 --        selector_expression  -- '.'
 --        argument_list  -- func call '(xxx)' 中的所有内容, 包括括号 ().
@@ -15,40 +47,36 @@ local function find_fn_call_before_cursor()
   local ts_status, ts_utils = pcall(require, "nvim-treesitter.ts_utils")
   if not ts_status then
     Notify("treesitter is not loaded.", "WARN")
-    return nil
+    return
   end
 
-  local pos = vim.fn.getpos('.')  -- 光标位置, 返回 [bufnum, lnum, col, off]. lnum 和 col 都是从 1 开始计算.
+  -- local pos = vim.fn.getpos('.')  -- 光标位置, 返回 [bufnum, lnum, col, off]. lnum 和 col 都是从 1 开始计算.
   local node = ts_utils.get_node_at_cursor()  -- 获取 node at cursor.
 
-  --- 向上寻找 'argument_list' / 'arguments' node. 'go, py' use argument_list; 'js, ts, lua' use arguments.
-  --- NOTE: foo("bar") 中 `("bar")` 属于 arguments, 包括括号.
+  --- 向上(parent)寻找 'argument_list' / 'arguments' node.
+  --- eg: fn("bar") 中 `("bar")` 属于 arguments, 包括括号.
+  --- NOTE: 'go, py' use 'argument_list'; 'js, ts, lua' use 'arguments'.
+  local args_type_name = {'argument_list', 'arguments'}
+
   while node do
-    if node:type() == 'argument_list' or node:type() == 'arguments' then
+    if vim.tbl_contains(args_type_name, node:type()) then
+      --- 判断 arguments/argument_list 前一个 node 是否 type_arguments(generic type param)
+      local prev_node = node:prev_named_sibling()
+      if prev_node:type() == 'type_arguments' then
+        local p_row, p_col, _ = prev_node:start()
+        print(p_row, p_col)
+
+        --- 如果是 type_arguments 则返回 type_arguments node 前一个字符位置.
+        return calculate_offset(p_row, p_col-1)
+      end
+
+      --- 如果前面不是 type_arguments 则返回本身 node 的前一个字符位置.
       local row, col, _ = node:start()  -- argument_list start position, 即 '(' 的位置. row, col 都是从 0 开始计算位置.
-      local func_call_last_char = col-1 -- VVI: col 是 '(' 的位置, func call() 是 '(' 前面的一个位置.
-
-      --- VVI: offset 中 \t 占4个字符位置, 而在 getpos() 和 node:start() 中只占1个字符.
-      --- strdisplaywidth() 会计算实际显示宽度, \t 会被计算在显示宽度之内.
-      --- argument_list 行 virtual column 位置.
-      local argDisplayCol = vim.fn.strdisplaywidth(string.sub(vim.fn.getline(row+1), 0, func_call_last_char))
-      --- cursor 行 virtual column 位置.
-      local curDisplayCol = vim.fn.strdisplaywidth(string.sub(vim.fn.getline(pos[2]), 0, pos[3]-1))
-      --- 计算 open_floating_preview() 横向偏移量
-      local offsetX = argDisplayCol - curDisplayCol
-
-      --- VVI:
-      --- char    - '(' arguments start() 的前一个位置.
-      --- offsetX - float_window 距离 cursor 的位置偏移.
-      --- offsetY - 同上, 由于 getpos() 返回的 lnum 是从 1 开始, 而 node:start() 返回的 row 是从 0 开始,
-      ---           所以 offsetY 的结果需要 +1.
-      return {line=row, char=func_call_last_char, offsetX=offsetX, offsetY=row-pos[2]+1}
+      return calculate_offset(row, col-1)
     end
 
     node = node:parent()  -- 循环向上查找, 如果到了 root node 也没找到的话, node:parent() 返回 nil.
   end
-
-  return nil
 end
 
 --- VVI: 自定义 lsp request ------------------------------------------------------------------------
@@ -114,8 +142,8 @@ M.hover_short = function()
     vim.lsp.util.make_position_params(),
     {
       position = {
-        character = result.char,  -- char 从 0 开始计算.
-        line = result.line,       -- line 从 0 开始计算.
+        line = result.lsp_req_pos_line,       -- line 从 0 开始计算.
+        character = result.lsp_req_pos_char,  -- char 从 0 开始计算.
       }
     }
   )
@@ -124,8 +152,8 @@ M.hover_short = function()
     --- VVI: 添加 offsetX 设置到 handler, 用来偏移 open_floating_preview() window
     vim.lsp.with(hover_short_handler,  -- VVI: 调用自定义 handler
       {
-        offset_x = result.offsetX,
-        offset_y = result.offsetY,
+        offset_x = result.offset_x,
+        offset_y = result.offset_y,
       }
     )
   )
