3dce8724f8b28cffc28f1070e70ed74881217b6f
crazytaxi <crazytaxi.git@outlook.com>
2022-08-20 23:22:29 +1000

    update: treesitter BUG Fixed

--- nvim/lua/user/plugin_settings/treesitter.lua
+++ nvim/lua/user/plugin_settings/treesitter.lua
@@ -28,8 +28,6 @@ ts_configs.setup {
     additional_vim_regex_highlighting = false,
   },
 
-  --- FIXME: 每次打开文件都会添加一个 setlocal indentexpr=nvim_treesitter#indent() --- {{{
-  --- https://github.com/nvim-treesitter/nvim-treesitter/issues/3172
   --- 使用 'indent_blankline' 代替.
   -- indent = {
   --   enable = true,
@@ -37,16 +35,15 @@ ts_configs.setup {
   -- },
 
   --- 作用不大.
-  --incremental_selection = {
-  --  enable = true,
-  --  keymaps = {
-  --    init_selection = "gnn",
-  --    node_incremental = "grn",
-  --    scope_incremental = "grc",
-  --    node_decremental = "grm",
-  --  },
-  --},
-  -- -- }}}
+  -- incremental_selection = {
+  --   enable = true,
+  --   keymaps = {
+  --     init_selection = "gnn",
+  --     node_incremental = "grn",
+  --     scope_incremental = "grc",
+  --     node_decremental = "grm",
+  --   },
+  -- },
 
   --- 启用第三方插件 modules 设置 ------------------------------------------------------------------
   --- "JoosepAlviste/nvim-ts-context-commentstring"
