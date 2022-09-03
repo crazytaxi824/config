e2868099231298002f19f86b0e95d40af4103308
crazytaxi <crazytaxi.git@outlook.com>
2022-09-03 17:17:57 +1000

    update: Check_cmd_tools()

--- nvim/lua/user/global_util/plugin_utils.lua
+++ nvim/lua/user/global_util/plugin_utils.lua
@@ -62,21 +62,49 @@ end
 ---   {cmd="vscode-json-language-server", lspconfig="jsonls", mason="json-lsp"}
 ---   {cmd="typescript-language-server", lspconfig="tsserver", mason="typescript-language-server"}
 ---   {cmd="dlv", mason="delve"}
+local function check_tool(tool, notify_opt)
+  if not tool.cmd or tool.cmd == '' then
+    notify_opt = vim.tbl_deep_extend('force', {timeout=false}, notify_opt)
+    Notify("check tool.cmd is missing", "ERROR", notify_opt)
+    return
+  end
+
+  local msg = {}
+  vim.fn.system('which '.. tool.cmd)
+  if vim.v.shell_error ~= 0 then
+    table.insert(msg, " - " .. tool.cmd)
+    if tool.mason then
+      table.insert(msg, "   - :MasonInstall " .. tool.mason)
+    end
+    if tool.install then
+      table.insert(msg, "   - " .. tool.install)
+    end
+  end
+
+  if #msg > 0 then
+    return msg
+  end
+end
+
 function Check_cmd_tools(tools, notify_opt)
   --- NOTE: "vim.schedule(function() ... end)" is a async function
   vim.schedule(function()
     local result = {"Tools should be installed:"}
     local count = 0
-    for _, tool in ipairs(tools) do
-      vim.fn.system('which '.. tool.cmd)
-      if vim.v.shell_error ~= 0 then
-        table.insert(result, " - " .. tool.cmd)
-        if tool.mason then
-          table.insert(result, "   - :MasonInstall " .. tool.mason)
+    if #tools > 0 then
+      --- list of tools
+      for _, tool in ipairs(tools) do
+        local msg = check_tool(tool, notify_opt)
+        if msg then
+          vim.list_extend(result, msg)
+          count = count + 1
         end
-        if tool.install then
-          table.insert(result, "   - " .. tool.install)
-        end
+      end
+    else
+      --- single tool
+      local msg = check_tool(tools, notify_opt)
+      if msg then
+        vim.list_extend(result, msg)
         count = count + 1
       end
     end
