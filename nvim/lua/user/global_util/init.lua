e04a5d55347388ef01555057b4e3126f0f152438
crazytaxi <crazytaxi.git@outlook.com>
2022-09-03 21:46:10 +1000

    add: user_commands

--- nvim/lua/user/global_util/init.lua
+++ nvim/lua/user/global_util/init.lua
@@ -1,11 +1,12 @@
---- VVI: 顺序很重要
-require("user.global_util.common_funcs")  -- 常用 functions
+require("user.global_util.common_funcs")
 require("user.global_util.notification")
 require("user.global_util.keymaps_register")
 require("user.global_util.check_tools")
-require("user.global_util.get_infos")
 require("user.global_util.jump_to_file")
 require("user.global_util.log_cotent_filter")
+
+require("user.global_util.user_commands")
+require("user.global_util.get_infos")
 
 
 
---
+++ nvim/lua/user/global_util/user_commands.lua
@@ -0,0 +1,26 @@
+--- delmakrs! 删除 buffer 中所有 marks, 除了 A-Z0-9
+vim.cmd('command -bar DelAllMarks delmarks! | delmarks A-Z0-9')
+
+--- BufLeave 切换窗口时触发.
+--- BufHidden 同一个窗口切换(load)不同 buffer 时触发.
+--- BufDelete :bdelete / :bwipeout 时触发.
+vim.api.nvim_create_autocmd({"BufDelete"},{
+  pattern = {"*"},
+  callback = function(params)
+    --- NOTE: 这里不能用 vim.cmd('delmarks! | delmarks A-Z0-9'), 因为 bnext | bd#
+    --- vim.api.nvim_buf_del_mark(params.buf, 'a')  -- a-z, local to buffer
+    --- vim.api.nvim_del_mark('A') A-Z0-9
+    for i=97,122,1 do
+      vim.api.nvim_buf_del_mark(params.buf, string.char(i))
+    end
+    for i=65,90,1 do
+      vim.api.nvim_del_mark(string.char(i))
+    end
+    for i=0,9,1 do
+      vim.api.nvim_del_mark(tostring(i))
+    end
+  end
+})
+
+
+
