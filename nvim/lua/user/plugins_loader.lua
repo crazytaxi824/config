432ad4ea04ed27486a100b540d6bed59510d0322
crazytaxi <crazytaxi.git@outlook.com>
2022-09-03 12:54:27 +1000

    upgrade: packer.nvim

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -205,7 +205,7 @@ packer.init {
 --- `:PackerSync` - install / update / clean 插件包.
 return packer.startup(function(use)
   use {"wbthomason/packer.nvim",  -- VVI: 必要. Have packer manage itself
-    commit = "3a9f980",
+    commit = "dee5a4f",
   }
 
   --- Performence & Functions ----------------------------------------------------------------------
