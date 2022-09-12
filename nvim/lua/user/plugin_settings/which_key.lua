7bc56b90805e9d5f5a970a7410c99191af3883af
crazytaxi <crazytaxi.git@outlook.com>
2022-09-12 22:38:21 +1000

    update: which-key

--- nvim/lua/user/plugin_settings/which_key.lua
+++ nvim/lua/user/plugin_settings/which_key.lua
@@ -3,6 +3,21 @@ if not status_ok then
   return
 end
 
+--- Shift + Ctrl + fn key Rename --- {{{
+local function fn_key_rename()
+  local fn_keys = {}
+
+  for i=1,12,1 do
+    fn_keys['<F' .. i+12 .. '>'] = '<S-F' .. i .. '>'
+    fn_keys['<F' .. i+24 .. '>'] = '<C-F' .. i .. '>'
+    fn_keys['<F' .. i+36 .. '>'] = '<C-S-F' .. i .. '>'
+  end
+
+  -- print(vim.inspect(fn_keys))
+  return fn_keys
+end
+-- -- }}}
+
 --- setup ---
 which_key.setup({
   plugins = {
@@ -16,33 +31,35 @@ which_key.setup({
     --- presets 是 which-key 预设好的 keymap 绑定. 虽然和系统快捷键功能一样, 但是进行了二次绑定, 所以会覆盖 keymaps.lua 中的设置.
     presets = {
       --- https://github.com/folke/which-key.nvim/blob/main/lua/which-key/plugins/presets/init.lua
-      operators = false,    -- adds help for operators like d, y, ... and registers them for motion / text object completion
-      motions = false,      -- adds help for motions, eg: 'gg', 'k', 'j', 'h', 'l', '0', '$', '^', 'b', 'e', 'w'...
-      text_objects = true,  -- help for text objects, eg: 'va' - around, 'vi' - inside ...
+      ---  BUG: operators 和 text_objects 中都有 'a', 'i' 设置, 重复显示.
+      operators = false,  -- 'c', 'd', 'y', 'v' ... 例如: 'ciw', 'caw', 'diw' ... NOTE: 设为 true 会影响 <, > buffer 跳转.
+      motions = false,   -- 'g', 例如: 'gg', 'ge' ...
+      text_objects = true,  -- 'a' - around, 'i' - inside.
+
       --- https://github.com/folke/which-key.nvim/blob/main/lua/which-key/plugins/presets/misc.lua
-      windows = true,       -- default bindings on <c-w>
-      nav = true,           -- '[', ']', 'H', 'M', 'L' ...
-      z = true,             -- bindings for folds, spelling and others prefixed with z
-      g = true,             -- bindings for prefixed with g. 'gn', 'gN', 'gi', 'gv', 'gx', 'gf', 'g%'
+      windows = true,  -- <c-w>
+      nav = true,  -- '[', ']' ...
+      z = true,    -- 'z=', 'zz', 'zo', 'zc' ...
+      g = true,    -- 'gn', 'gN', 'gi', 'gv', 'gx', 'gf', 'g%'
     },
   },
   operators = { gc = "Comments" },  -- NOTE: 手动 trigger which-key 显示.
-  key_labels = {
-    --- override the label used to display some keys. It doesn't effect WK in any other way.
-    --- For example:
-    --  ["<space>"] = "SPC",
-    --  ["<cr>"] = "RET",
-    --  ["<tab>"] = "TAB",
-  },
+
+  --- NOTE: override the label used to display some keys.
+  --- 这里是将 <F24> rename 到 <S-F12> ...
+  key_labels = fn_key_rename(),
+
   icons = {
     breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
     separator = "➜",  -- symbol used between a key and it's label
-    group = "+ ",     -- symbol prepended to a group
+    group = "+",     -- symbol prepended to a group
   },
   popup_mappings = {
     scroll_down = '<down>', -- <C-d> binding to scroll down inside the popup
     scroll_up = '<up>', -- <C-u> binding to scroll up inside the popup
   },
+
+  --- NOTE: 窗口显示设置.
   window = {
     border = "none", -- none, single, double, shadow
     position = "bottom", -- bottom, top
@@ -56,9 +73,11 @@ which_key.setup({
     spacing = 3, -- spacing between columns
     align = "left", -- align columns left, center or right
   },
+
   ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
   hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ "}, -- hide mapping boilerplate
-  show_help = true, -- show help message 在下方 <BSpace> 可以返回上一层菜单.
+  show_help = true, -- show help message 在下方 <bs> 返回上一层菜单, <esc> 退出.
+
   triggers = "auto", -- automatically setup triggers
   --triggers = {"<leader>"} -- or specify a list manually
   triggers_blacklist = {
@@ -67,6 +86,13 @@ which_key.setup({
     --- most people should not need to change this
     i = { "j", "k" },
     v = { "j", "k" },
+  },
+
+  -- disable the WhichKey popup for certain buf types and file types.
+  -- Disabled by deafult for Telescope
+  disable = {
+    buftypes = {},
+    filetypes = { "TelescopePrompt" },
   },
 })
 
