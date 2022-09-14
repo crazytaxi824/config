65d7fcfb7c47fa12f4994dabfd2772f192d72aed
crazytaxi <crazytaxi.git@outlook.com>
2022-09-15 09:44:02 +1000

    update: GoTests -> GoTestsGenerator

--- nvim/after/ftplugin/go/tool_gotests.lua
+++ nvim/after/ftplugin/go/tool_gotests.lua
@@ -31,7 +31,7 @@ end
 -- command! -buffer -nargs=0 GoTests :lua _GoTests()
 vim.api.nvim_buf_create_user_command(
   0,          -- bufnr = 0 表示 current buffer.
-  "GoTests",  -- command name
+  "GoTestsGenerator",  -- command name
   gotests_cmd_tool,    -- 使用 vim.api 方法的好处是可以使用 local lua function
   {bang = true, bar = true}  -- options: {bang = true, nargs = "+"}
 )
