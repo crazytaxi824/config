6a87848b8dad3741b730d9bce7971c12940db25a
crazytaxi <crazytaxi.git@outlook.com>
2022-09-16 20:09:09 +1000

    chore: move impl from after/ftplugin to user/ftplugin_tools

--- nvim/after/ftplugin/go/tool_impl.lua
+++ nvim/after/ftplugin/go/tool_impl.lua
@@ -1,61 +1,9 @@
---- NOTE: <cword> under the cursor need to be a interface name.
---  `impl -dir src Cat IAnimal`
---      - 'Cat'      是我们需要定义的 type, 也就是以下传入的 obj.
---      - 'Animal'   是 interface 的名字, 使用时需要将 cursor 放在 Animal 上.
---  `:call writefile(["foo"], "src/main.go", "a")`  -- 'a': append mode, 将数据写入文件最后.
---
---  操作方法, cursor 指向 interface Name <cword>, 使用 Command `:GoImpl Foo`
-local function go_impl(arglist)
-  if vim.bo.readonly then
-    Notify("this is a readonly file","ERROR")
-    return
-  end
-
-  if #arglist > 1 then
-    Notify({"only one args is allowed", "  :GoImpl Foo"},"ERROR")
-    return
-  end
-
-  local dir = vim.fn.expand('%:h')
-  local lastline = vim.fn.line('$')
-  local iface_name = vim.fn.expand('<cword>')
-
-  --- 执行 shell cmd
-  local sh_cmd = 'impl -dir ' .. dir .. ' ' .. arglist[1] .. ' ' .. iface_name
-  print(sh_cmd)
-  local result = vim.fn.system(sh_cmd)
-
-  --- 判断结果是否错误
-  if vim.v.shell_error ~= 0 then
-    Notify(result,"ERROR")
-    return
-  end
-
-  --- 删除 result 最后的空行.
-  local content = vim.split(result, '\n')
-  while content[#content] == '' do
-    table.remove(content, #content)
-  end
-  print(vim.inspect(content))
-
-  --- add 'type Foo struct{}'
-  local msg = vim.list_extend({"", "type " .. arglist[1] .. " struct{}", ""}, content)
-
-  --- 写入当前文件
-  vim.fn.writefile(msg, vim.fn.expand('%'), 'a')  -- 'a' append mode
-
-  --- checktime          刷新 buffer 显示
-  --- cursor(lnum. col)  移动 cursor
-  --- zz                 移动 cursor 行到屏幕中间
-  vim.cmd('checktime | call cursor('.. lastline+2 ..', 1) | normal! zz')
-end
-
 --- command! -buffer -nargs=1 GoImpl :lua _GoImpl(<f-args>)
 vim.api.nvim_buf_create_user_command(
   0,
   "GoImpl",
   function(params)
-    go_impl(params.fargs)
+    require("user.ftplugin_tools.go").go_impl(params.fargs)
   end,
   {bang=true, nargs="+"}
 )
--- nvim/lua/user/ftplugin_tools/go/init.lua
+++ nvim/lua/user/ftplugin_tools/go/init.lua
@@ -1,8 +1,9 @@
 local go_run = require("user.ftplugin_tools.go.go_run")
 local go_test_single = require("user.ftplugin_tools.go.go_test_single")
 local go_test_pkg = require("user.ftplugin_tools.go.go_test_pkg")
-local go_modifytags = require("user.ftplugin_tools.go.tool_gomodifytags")
+local gomodifytags = require("user.ftplugin_tools.go.tool_gomodifytags")
 local gotests = require("user.ftplugin_tools.go.tool_gotests")
+local impl = require("user.ftplugin_tools.go.tool_impl")
 
 local M = {
   --- methods
@@ -14,11 +15,12 @@ local M = {
   go_test_bench_pkg = go_test_pkg.go_test_bench_pkg,
   go_test_bench_proj = go_test_pkg.go_test_bench_proj,
 
-  go_add_tags_and_opts = go_modifytags.go_add_tags_and_opts,
-  go_remove_tags = go_modifytags.go_remove_tags,
-  go_remove_tags_opts = go_modifytags.go_remove_tags_opts,
+  go_add_tags_and_opts = gomodifytags.go_add_tags_and_opts,
+  go_remove_tags = gomodifytags.go_remove_tags,
+  go_remove_tags_opts = gomodifytags.go_remove_tags_opts,
 
   gotests_cmd_tool = gotests.gotests_cmd_tool,
+  go_impl = impl.go_impl,
 }
 
 return M
