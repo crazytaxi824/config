e7e0bb2d95d2fc6bec698e1bfa45bc287b0ff8f6
crazytaxi <crazytaxi.git@outlook.com>
2022-09-16 20:03:26 +1000

    chore: move gotests from after/ftplugin to user/ftplugin_tools

--- nvim/lua/user/ftplugin_tools/go/init.lua
+++ nvim/lua/user/ftplugin_tools/go/init.lua
@@ -2,6 +2,7 @@ local go_run = require("user.ftplugin_tools.go.go_run")
 local go_test_single = require("user.ftplugin_tools.go.go_test_single")
 local go_test_pkg = require("user.ftplugin_tools.go.go_test_pkg")
 local go_modifytags = require("user.ftplugin_tools.go.tool_gomodifytags")
+local gotests = require("user.ftplugin_tools.go.tool_gotests")
 
 local M = {
   --- methods
@@ -16,6 +17,8 @@ local M = {
   go_add_tags_and_opts = go_modifytags.go_add_tags_and_opts,
   go_remove_tags = go_modifytags.go_remove_tags,
   go_remove_tags_opts = go_modifytags.go_remove_tags_opts,
+
+  gotests_cmd_tool = gotests.gotests_cmd_tool,
 }
 
 return M
