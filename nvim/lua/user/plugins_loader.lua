0ac9bf8f1c6383c6924b0224325d4ad67fa79969
crazytaxi <crazytaxi.git@outlook.com>
2022-09-21 14:38:02 +1000

    upgrade: bufferline

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -433,7 +433,7 @@ return packer.startup(function(use)
   --- Buffer & Status Line -------------------------------------------------------------------------
   --- tabline decorator, `:help 'tabline'`
   use {"akinsho/bufferline.nvim",
-    commit = "ffa1d00",
+    commit = "83bf4dc",  -- TODO: refactor
     config = function() require("user.plugin_settings.decor_bufferline") end,
     opt = true,  -- 在 vim.schedule() 中 lazy load
   }
