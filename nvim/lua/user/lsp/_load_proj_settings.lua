1920b81ce90a1ff3b563e47facba2fb13f00126b
crazytaxi <crazytaxi.git@outlook.com>
2022-10-08 23:04:05 +1000

    feat: find '.nvim/settings.lua' file from pwd to root

--- nvim/lua/user/lsp/_load_proj_settings.lua
+++ nvim/lua/user/lsp/_load_proj_settings.lua
@@ -12,21 +12,44 @@ local M = {}
 local once = nil    -- 是否已经读取过 file. true - 已经读取过, false|nil - 未读取 file.
 local content = {}  -- 缓存 file 内容. VVI: 这里不要使用 nil, 因为 nil 无法 index [lsp] / [null-ls].
 
+--- 从 pwd 向上获取 dir 直到 root "/".
+local function local_setting_dir_filepaths_to_root()
+  local absolute_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':p')
+  local path_slice = vim.split(absolute_dir, '/', {trimempty=true})
+  local path_len = #path_slice
+
+  local t = {}
+  for i = path_len, 1, -1 do
+    table.insert(t, '/' .. table.concat(path_slice, '/') .. '/')
+    table.remove(path_slice, i)
+  end
+
+  table.insert(t, '/')  -- root dir
+
+  return t
+end
+
 --- 内部函数 lazyload() 保证只读取一次文件.
 local function lazyload_local_settings()
   --- 如果已经读取文件则不重复执行.
   if once then
-    -- print('project local settings are already loaded (once)')
     return
   end
-  -- print('load project local settings')
 
-  --- 第一次读取文件, NOTE: 使用 dofile 方法执行指定 lua 文件.
+  local dirs = local_setting_dir_filepaths_to_root()
+
   --- 如果文件不存在, 或执行错误(语法错误), 则忽略.
-  local ok, local_settings = pcall(dofile, '.nvim/settings.lua')
-  if ok and local_settings then
-    --- '.nvim/settings.lua' 读取成功, 同时返回值不是 nil 的情况下缓存数据
-    content = local_settings  -- 缓存数据.
+  for _, dir in ipairs(dirs) do
+    local local_settings_filepath = dir .. '.nvim/settings.lua'
+    if vim.fn.filereadable(local_settings_filepath) then
+      --- 使用 pcall 确保 lua file 执行没有错误.
+      local ok, local_settings = pcall(dofile, local_settings_filepath)
+      if ok and local_settings then
+        --- '.nvim/settings.lua' 读取成功, 同时返回值不是 nil 的情况下缓存数据
+        content = local_settings  -- 缓存数据.
+        break
+      end
+    end
   end
 
   once = true  -- 标记为已读.
