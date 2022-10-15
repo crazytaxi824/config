523c27a2d28d8bc641585bc20d6911ae3cc1857d
crazytaxi <crazytaxi.git@outlook.com>
2022-10-15 22:47:19 +1100

    upgrade: LuaSnip

--- nvim/lua/user/plugin_settings/luasnip_snippest.lua
+++ nvim/lua/user/plugin_settings/luasnip_snippest.lua
@@ -6,6 +6,7 @@ end
 
 --- "L3MON4D3/LuaSnip" 设置 ------------------------------------------------------------------------
 --- https://github.com/L3MON4D3/LuaSnip/blob/master/Examples/snippets.lua
+--- https://github.com/L3MON4D3/LuaSnip#config
 luasnip.config.set_config({
   history = false,
 })
@@ -14,9 +15,10 @@ luasnip.config.set_config({
 --- 默认加载 `:set runtimepath?` 中的 package.json 文件. NOTE: package.json, go.json ... 不能有注释 否则无法解析.
 --- 可以参照 https://github.com/rafamadriz/friendly-snippets 自己定义 snippets
 -- -- }}}
---- vscode 指定读取 "~/.config/nvim/snip/package.json"
+--- NOTE: `:help luasnip-loaders`
 vim.schedule(function()
   require("luasnip.loaders.from_vscode").lazy_load({
+    --- 指定读取 "~/.config/nvim/snip/package.json"
     --- 这里是加载自定义 snippets, 地址是 runtimepath/snip/package.json
     --- 这里的 runtimepath 是 ~/.config/nvim/
     paths = {"./snip"},  -- 这里的路径是相对于 runtimepath
