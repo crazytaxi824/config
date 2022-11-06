69415ab946c981eb91a8a8acc8d18e4d2ca88038
crazytaxi <crazytaxi.git@outlook.com>
2022-11-06 20:28:52 +1100

    comments

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -468,7 +468,7 @@ return packer.startup(function(use)
   --- Useful Tools ---------------------------------------------------------------------------------
   --- fzf rg fd, preview 使用的是 treesitter, 而不用 bat
   use {"nvim-telescope/telescope.nvim",
-    commit = "4bd4205",  -- TODO: feat: allow table as additional args in live grep and grep string (#2139)
+    commit = "4bd4205",
     config = function() require("user.plugin_settings.telescope_fzf") end,
     requires = {
       "nvim-lua/plenary.nvim",
