0a6da49517cf1a2e59727bd1380f76308ff3401e
crazytaxi <crazytaxi.git@outlook.com>
2022-11-07 22:08:22 +1100

    comments

--- nvim/after/queries/go/highlights.scm
+++ nvim/after/queries/go/highlights.scm
@@ -1,10 +1,10 @@
 ; extends
 ;;; `:help treesitter-predicates`
 
-;;; go struct private field
-;;; 如果 field_declaration 中的 field_identifier 名字是小写开头, 则定义为 @field.private,
-;;; 需要在 colors.lua 中定义 @field.private 颜色.
-;;; type Foo struct { bar string } 中的 bar.
+;;; go struct private field 颜色, eg: type Foo struct { bar string } 中的 bar.
+;;; 可以通过 `:TSPlaygroundToggle` 查看 node 属性, 得到 field_identifier | field_declaration ...
+;;; 如果 "field_declaration" 中的 "name: field_identifier" 名字是小写开头, 则定义为 @field.private,
+;;; NOTE: 需要在 colors.lua 中定义 @field.private 颜色.
 (field_declaration name: ((field_identifier) @field.private
   (#lua-match? @field.private "^[a-z]")))
 
