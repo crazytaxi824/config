da74bd8d458a61be836304c46ee502cb71e043b4
crazytaxi <crazytaxi.git@outlook.com>
2022-12-05 16:19:03 +1100

    update: impatient settings

--- nvim/lua/user/plugin_settings/impatient.lua
+++ nvim/lua/user/plugin_settings/impatient.lua
@@ -1,15 +1,23 @@
 --- "lewis6991/impatient.nvim" settings --------------------------------------------------------------
---- VVI: This must be populated before require('impatient') is run.
-_G.__luacache_config = {     --- this is a Global var.
-  chunks = {
-    enable = true,
-    path = vim.fn.stdpath('cache')..'/luacache_chunks',
-  },
-  modpaths = {
-    enable = true,
-    path = vim.fn.stdpath('cache')..'/luacache_modpaths',
+--- DOC: https://github.com/lewis6991/impatient.nvim#configuration
+
+local luacache_chunks = vim.fn.stdpath('cache')..'/luacache_chunks'
+local luacache_modpaths = vim.fn.stdpath('cache')..'/luacache_modpaths'
+
+--- 判断 luacache 文件是否存在.
+if vim.fn.filereadable(luacache_chunks) == 1 and vim.fn.filereadable(luacache_modpaths) == 1 then
+  --- VVI: This must be populated before require('impatient') is run.
+  _G.__luacache_config = {     --- this is a Global var.
+    chunks = {
+      enable = true,
+      path = luacache_chunks,
+    },
+    modpaths = {
+      enable = true,
+      path = luacache_modpaths,
+    }
   }
-}
+end
 
 --- VVI: it is recommended you add the following near the start of your init.vim.
 local status_ok, impatient = pcall(require, "impatient")
