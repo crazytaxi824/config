cd402d4e1d8996d5d9c05ad8f96f27c38b2a22ac
crazytaxi <crazytaxi.git@outlook.com>
2022-12-08 08:50:25 +1100

    feat: add github/copilot.nvim

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -606,6 +606,11 @@ return packer.startup(function(use)
   --use "goolord/alpha-nvim"          -- neovim 启动页面
   --use "ahmedkhalf/project.nvim"     -- project manager
 
+  --- https://docs.github.com/en/enterprise-cloud@latest/copilot/getting-started-with-github-copilot/getting-started-with-github-copilot-in-neovim
+  use {"github/copilot.vim",  -- NOTE: 目前不需要锁定版本.
+    cmd = {"Copilot"},  -- `:Copilot setup`, `:help copilot` 查看可用命令.
+  }
+
   --- VVI: 需要安装 packer 的情况下, 同时安装其他插件.
   if packer_bootstrap then
     packer.sync()
