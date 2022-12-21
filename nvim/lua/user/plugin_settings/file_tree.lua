464e73ae495ff9512e4bf813b925da77883d27c8
crazytaxi <crazytaxi.git@outlook.com>
2022-12-21 23:39:11 +1100

    update: file_tree settings

--- nvim/lua/user/plugin_settings/file_tree.lua
+++ nvim/lua/user/plugin_settings/file_tree.lua
@@ -269,7 +269,7 @@ nvim_tree.setup {
   auto_reload_on_write = true,  -- NOTE: `:w` 时刷新 nvim-tree.
 
   --- NOTE: netrw: vim's builtin file explorer.
-  disable_netrw = open_dir_in_tree,  -- NOTE: completely disable netrw.
+  disable_netrw = false,  -- NOTE: completely disable netrw.
 
   --- true  - `:e dir` 时, 不显示 netrw file explorer.
   --- false - `:e dir` 时, 在当前 window 中显示 netrw file explorer.
@@ -496,20 +496,6 @@ vim.api.nvim_create_autocmd({"BufEnter", "BufDelete"}, {
     end)
   end
 })
-
---- NOTE: 如果 open_dir_in_tree = false, 则 `setlocal nobuflisted` to dirctory buffer.
---- 在 nvim-tree 设置 hijack netrw & directories = false 的情况下使用.
-if not open_dir_in_tree then
-  vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = {"*"},
-    callback = function(params)
-      if vim.fn.isdirectory(params.file) == 1 then
-        --- setlocal nobuflisted
-        vim.bo[params.buf].buflisted = false
-      end
-    end
-  })
-end
 
 -- -- }}}
 
