6cde0db66c24f015c006c5768c5676b829272cd0
crazytaxi <crazytaxi.git@outlook.com>
2022-12-22 13:56:05 +1100

    chore: glboal

--- nvim/lua/user/global/init.lua
+++ nvim/lua/user/global/init.lua
@@ -1,9 +1,9 @@
-require("user.global.deps.common_funcs")
-require("user.global.deps.notification")
+require("user.global.fzf_edit") -- VVI: 用于编辑 fzf multi selected item
 
-require("user.global.utils.debug_autocmd")
-require("user.global.utils.fzf_edit")
+require("user.global.common_funcs")
+require("user.global.notification")
 
+require("user.global.cmds_autocmds.debug_autocmd")
 require("user.global.cmds_autocmds.log_cotent_filter")
 require("user.global.cmds_autocmds.del_marks")
 
