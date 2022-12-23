63c586874a19c960cf15184c8d0aeb52dacb9837
crazytaxi <crazytaxi.git@outlook.com>
2022-12-24 00:14:11 +1100

    update: toggleterm

--- nvim/lua/user/plugin_settings/toggleterm_terminal.lua
+++ nvim/lua/user/plugin_settings/toggleterm_terminal.lua
@@ -119,24 +119,15 @@ function _Exec(cmd, on_exit_fn)
       buffer = 0,
       callback = function(params)
         --- 如果 goto 的 win_id 不存在, 则会自动跳到别的 window.
-        vim.fn.win_gotoid(exec_win_id)
-        vim.wo[exec_win_id].cursorline = true  -- VVI: 必须要, 因为这里无法触发 WinEnter, 导致 cursorline 无法设置.
+        if vim.fn.win_gotoid(exec_win_id) == 1 then
+          --- VVI: 必须要, 因为这里无法触发 WinEnter, 导致 cursorline 无法设置.
+          vim.wo[exec_win_id].cursorline = true
+        end
 
         --- 删除 augroup
         vim.api.nvim_del_augroup_by_id(group_id)
       end,
       desc = 'go back to window which execute _Exec()',
-    })
-
-    vim.api.nvim_create_autocmd("WinClosed", {
-      group = group_id,
-      buffer = 0,
-      callback = function(params)
-        --- 如果 goto 的 win_id 不存在, 则会自动跳到别的 window.
-        vim.fn.win_gotoid(exec_win_id)
-        vim.wo[exec_win_id].cursorline = true  -- VVI: 必须要, 因为这里无法触发 WinEnter, 导致 cursorline 无法设置.
-      end,
-      desc = 'back to prev window which execute _Exec()',
     })
   end
 
