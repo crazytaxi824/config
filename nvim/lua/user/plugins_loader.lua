c12ab7980853727dfb73af841fab3b73413da4c7
crazytaxi <crazytaxi.git@outlook.com>
2022-12-29 17:16:19 +1100

    upgrade: (lualine)

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -521,7 +521,7 @@ return require('packer').startup(function(use)
 
   --- statusline decorator, `:help 'statusline'`
   use {"nvim-lualine/lualine.nvim",   -- bottom status line
-    commit = "bfa0d99",
+    commit = "32a7382",
     config = function() require("user.plugin_settings.decor_lualine") end,
 
     opt = true,  -- 在 vim.schedule() 中 lazy load
