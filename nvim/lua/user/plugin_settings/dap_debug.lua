1d84474bcd104a5f5e855be087856fe09162185c
crazytaxi <crazytaxi.git@outlook.com>
2023-02-09 14:05:42 +1100

    update: (dapui) add highlight 'DapUINormal'

--- nvim/lua/user/plugin_settings/dap_debug.lua
+++ nvim/lua/user/plugin_settings/dap_debug.lua
@@ -96,9 +96,12 @@ vim.api.nvim_set_hl(0, 'DapStoppedLineHL', { ctermbg = 238 })
 vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpointHL", numhl = "", linehl="" })
 vim.fn.sign_define("DapBreakpointRejected", { text = "◌", texthl = "DapBreakpointRejectedHL", numhl = "", linehl="" })
 vim.fn.sign_define("DapStopped", { text = "→", texthl = "DapStoppedHL", numhl = "", linehl="DapStoppedLineHL" })
+
+--vim.api.nvim_set_hl(0, 'DapUINormal', { ctermbg = 190 })  -- dapui 中的文字颜色.
 -- -- }}}
 
 --- nvim-dap-ui settings ---------------------------------------------------------------------------
+--- `:help dapui.setup()`
 dapui.setup({
   mappings = {
     expand = {"<CR>", "<2-LeftMouse>"}, -- Use a table to apply multiple mappings
