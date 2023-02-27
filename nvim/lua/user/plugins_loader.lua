ce46240bc181a91c3b740a1a3a519ca7d148ebfb
crazytaxi <crazytaxi.git@outlook.com>
2023-02-28 09:52:24 +1100

    upgrade: (indentline)

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -377,8 +377,7 @@ return require('packer').startup(function(use)
 
   --- indent line
   use {"lukas-reineke/indent-blankline.nvim",
-    -- commit = "8299fe7",  -- version 2.20.3 == commit '8299fe7'
-    tag = '*',  -- '*' means "latest tag". NOTE: 该插件更新不频繁, 可以使用最新 tag.
+    tag = 'v2.20.4',  -- commit = "018bd04",
     config = function() require("user.plugin_settings.indentline") end,  -- setup() 设置 use_treesitter = true
     requires = "nvim-treesitter/nvim-treesitter",
 
