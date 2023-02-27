a5a56e057f37db0111f8a0a9bb1a9e2447a1b848
crazytaxi <crazytaxi.git@outlook.com>
2023-02-28 09:31:20 +1100

    upgrade: (plenary)

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -283,7 +283,7 @@ return require('packer').startup(function(use)
   --- Useful lua functions used by lots of plugins
   --- NOTE: plenary.nvim 合并了 popup.nvim
   use {"nvim-lua/plenary.nvim",
-    commit = "9a0d3bf",
+    commit = "253d348",
   }
 
   --- Must install ---------------------------------------------------------------------------------
