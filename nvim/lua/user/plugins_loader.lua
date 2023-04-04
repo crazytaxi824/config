6b0e7e003ae0cc6c8d8abafea4a9032f762821b4
crazytaxi <crazytaxi.git@outlook.com>
2023-04-05 09:40:24 +1000

    upgrade: (which-key) v1.2.1 fixbugs

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -289,7 +289,7 @@ return require('packer').startup(function(use)
   --- Must install ---------------------------------------------------------------------------------
   --- 快捷键提醒功能, key mapping 的时候需要注册到 which-key
   use {"folke/which-key.nvim",
-    tag = "v1.2.0",
+    tag = "v1.2.1",
     config = function() require("user.plugin_settings.which_key") end,
   }
 
