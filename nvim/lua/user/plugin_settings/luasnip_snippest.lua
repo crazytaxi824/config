3e74e6cda4a831bb3f861cc4f13e54ca93d8a58f
crazytaxi <crazytaxi.git@outlook.com>
2023-04-05 10:40:14 +1000

    upgrade: (LuaSnip)

--- nvim/lua/user/plugin_settings/luasnip_snippest.lua
+++ nvim/lua/user/plugin_settings/luasnip_snippest.lua
@@ -6,11 +6,11 @@ end
 
 --- "L3MON4D3/LuaSnip" 设置 ------------------------------------------------------------------------
 --- https://github.com/L3MON4D3/LuaSnip/blob/master/Examples/snippets.lua
---- https://github.com/L3MON4D3/LuaSnip#config
-luasnip.config.set_config({
-  history = false,
-  --update_events = 'TextChanged,TextChangedI', -- 默认: 'InsertLeave'
-})
+--- `:help luasnip-config-options`
+-- luasnip.setup({
+--   history = false,
+--   update_events = 'TextChanged,TextChangedI',
+-- })
 
 --- VVI: 读取配置文件地址 --- {{{
 --- 默认加载 `:set runtimepath?` 中的 package.json 文件. NOTE: package.json, go.json ... 不能有注释 否则无法解析.
@@ -18,13 +18,14 @@ luasnip.config.set_config({
 -- -- }}}
 --- NOTE: `:help luasnip-loaders`
 require("luasnip.loaders.from_vscode").lazy_load({
-  --- 指定读取 "~/.config/nvim/snip/package.json"
+  --- NOTE: 自定义 snippets. 路径是相对于 runtimepath, 读取 paths 内 "package.json" 文件.
+  --- 这里读取的是 "~/.config/nvim/snip/package.json" 文件.
   --- 这里是加载自定义 snippets, 地址是 runtimepath/snip/package.json
   --- 这里的 runtimepath 是 ~/.config/nvim/
-  paths = {"./snip"},  -- 这里的路径是相对于 runtimepath
+  paths = {"./snip"},
 })
 require("luasnip.loaders.from_vscode").lazy_load({
-  --- paths 缺省时自动加载 runtimepath/package.json 这里是加载 friendly-snippets
+  --- NOTE: paths 缺省时自动加载 runtimepath/package.json 这里是加载 friendly-snippets
   --- 这里的 runtimepath 是 ~/.local/share/nvim/site/pack/*/start/*/,
   --- 即: ~/.local/share/nvim/site/pack/packer/start/friendly-snippets/
   --paths = {},
