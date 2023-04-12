8b74218cb191ec20579fe665648df09cf02e3aec
crazytaxi <crazytaxi.git@outlook.com>
2023-04-12 15:18:27 +1000

    upgrade: (dap, dap-ui)

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -526,14 +526,14 @@ return require('packer').startup(function(use)
   --- Debug tools 安装 -----------------------------------------------------------------------------
   --- NOTE: dap-ui && dap 设置在同一文件中.
   use {"mfussenegger/nvim-dap",  -- core debug tool
-    tag = "0.5.0",
+    tag = "0.6.0",
     requires = "williamboman/mason.nvim",  -- install dap-debug tools. eg: 'delve'
 
     cmd = {'DapToggleBreakpoint', 'DapContinue', 'DapLoadLaunchJSON'},
   }
 
   use {"rcarriga/nvim-dap-ui",  -- ui for "nvim-dap"
-    tag = "v3.8.0",
+    commit = "286f682",  -- tag = "v3.8.0",
     config = function() require("user.plugin_settings.dap_debug") end,  -- dap-ui && dap 设置在同一文件中.
 
     after = "nvim-dap",
