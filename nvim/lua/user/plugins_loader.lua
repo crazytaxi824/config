6d9216a167eba8b266360cdc9e1663d737cdae63
crazytaxi <crazytaxi.git@outlook.com>
2023-05-01 18:18:04 +1000

    upgrade: (toggleterm)

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -558,7 +558,7 @@ return require('packer').startup(function(use)
 
   --- terminal
   use {"akinsho/toggleterm.nvim",
-    tag = "v2.5.0",
+    tag = "v2.6.0",
     config = function() require("user.plugin_settings.toggleterm_terminal") end,
 
     opt = true,  -- 在 vim.schedule() 中 lazy load
