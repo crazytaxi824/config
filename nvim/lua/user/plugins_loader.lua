344be170688ae59f7114b6dda16e8ca6122e702e
crazytaxi <crazytaxi.git@outlook.com>
2023-05-01 18:15:51 +1000

    upgrade: (telescope)

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -542,7 +542,7 @@ return require('packer').startup(function(use)
   --- Useful Tools ---------------------------------------------------------------------------------
   --- fzf rg fd, preview 使用的是 treesitter, 而不用 bat
   use {"nvim-telescope/telescope.nvim",
-    commit = "6258d50",
+    commit = "713d26b",
     config = function() require("user.plugin_settings.telescope_fzf") end,
     requires = {
       "nvim-lua/plenary.nvim",
