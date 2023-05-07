9a5f1471b718d65a688d021cb45b3ac0c9b3597d
crazytaxi <crazytaxi.git@outlook.com>
2023-05-07 17:46:09 +1000

    upgrade: (snippets)

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -441,7 +441,7 @@ return require('packer').startup(function(use)
 
   --- snippets content, 自定义 snippets 可以借鉴这个结构.
   use {"rafamadriz/friendly-snippets",
-    commit = "b2ce8ab",
+    commit = "10f7419",
     --- VVI: friendly-snippets 不能安装在 opt/ 文件夹下, 否则不生效.
   }
 
