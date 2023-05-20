bbc4c64b146d562e482acd0b8e2393ee71a468ab
crazytaxi <crazytaxi.git@outlook.com>
2023-05-21 09:59:24 +1000

    upgrade: (cmp)

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -387,7 +387,7 @@ return require('packer').startup(function(use)
 
   --- Auto Completion ------------------------------------------------------------------------------
   use {"hrsh7th/nvim-cmp",
-    commit = "1cad30f",
+    commit = "3ac8d6c",
     config = function() require("user.plugin_settings.cmp_completion") end,
     requires = {
       "hrsh7th/cmp-nvim-lsp",  -- lsp 提供的代码补全
