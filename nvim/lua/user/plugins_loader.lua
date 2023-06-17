45bd0539a37cad76e30e2732466a067c04d10ac5
crazytaxi <crazytaxi.git@outlook.com>
2023-06-17 10:24:35 +1000

    upgrade(treesitter-context)

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -339,7 +339,7 @@ return require('packer').startup(function(use)
 
   --- 第一方 module 插件 ---
   use {"nvim-treesitter/nvim-treesitter-context",  -- 顶部显示 cursor 所在 function 的定义.
-    commit = "e2ea376",
+    commit = "efe8706",
     config = function() require("user.plugin_settings.treesitter_ctx") end,
 
     after = "nvim-treesitter",
