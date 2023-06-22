c236f4dbe5f4183955943da14767db13bbf02ea5
crazytaxi <crazytaxi.git@outlook.com>
2023-06-22 11:51:14 +1000

    upgrade(gitsigns)

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -572,7 +572,7 @@ return require('packer').startup(function(use)
   --- `:Gitsigns setqflist/seqloclist` will open Trouble instead of quickfix or location list windows.
   --- https://github.com/lewis6991/gitsigns.nvim#troublenvim
   use {"lewis6991/gitsigns.nvim",
-    commit = "256569c",
+    commit = "a36bc33",
     config = function() require("user.plugin_settings.git_signs") end,
 
     opt = true,  -- 在 vim.schedule() 中 lazy load
