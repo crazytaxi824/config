c713a1d3a4f71ea03766c6959ab6e42fbbc8d340
crazytaxi <crazytaxi.git@outlook.com>
2023-07-04 09:49:55 +1000

    upgrade(null-ls): fix bugs

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -487,7 +487,7 @@ local plugins = {
 
   --- null-ls 插件 formatters && linters, depends on "nvim-lua/plenary.nvim"
   {"jose-elias-alvarez/null-ls.nvim",
-    commit = "aac27a1",
+    commit = "608547d",
     config = function() require("user.lsp.null_ls") end,
     requires = {
       "nvim-lua/plenary.nvim",
