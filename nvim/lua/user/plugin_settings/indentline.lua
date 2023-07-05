6e2b2277263800aba319965ee4297268015ecd04
crazytaxi <crazytaxi.git@outlook.com>
2023-07-05 10:41:24 +1000

    update(indentline)

--- nvim/lua/user/plugin_settings/indentline.lua
+++ nvim/lua/user/plugin_settings/indentline.lua
@@ -12,10 +12,9 @@ end
 --vim.opt.listchars:append("eol:↴")
 
 --- `:help indent-blankline`
+--- 设置参考 LunarVim: https://github.com/LunarVim/LunarVim/blob/master/lua/lvim/core/indentlines.lua
 indent_blankline.setup({
   enabled = true,
-  --use_treesitter = true,  -- NOTE: use treesitter when possible. 默认 false.
-  --use_treesitter_scope = true,  -- VVI: 不要设置, 会导致 indentline 显示不如预期.
 
   indent_level = 9,  -- VVI: maximum indent level to display. 默认 10.
   max_indent_increase = 1,  -- 多行 trailing comments 不会出现 indentline.
@@ -36,17 +35,19 @@ indent_blankline.setup({
     --"python",  -- python 不适合 indent line.
   },
 
-  char = "│",  -- 默认 "▏" and "▎"
-  context_char = '┃',  -- 默认为 char 的值. 如果 "show_current_context = false" 则本设置无效.
+  char = "│",  -- "│", "┃" and "▏", "▎",
+  context_char = '│',  -- 如果 "show_current_context = false" 则本设置无效.
 
-  show_trailing_blankline_indent = false,  -- VVI: ) } 后的空白行不再显示 indent line.
   show_first_indent_level = true,  -- 显示第一列的 indent line.
+  show_trailing_blankline_indent = false,  -- VVI: ) } 后的空白行不再显示 indent line.
   --space_char_blankline = ' ',  -- indent line 之间的空白显示, 默认为空字符 ' '.
   --show_end_of_line = true,  -- 同时需要设置 vim.opt.listchars:append("eol:↴")
 
   --- NOTE: 以下设置需要安装 nvim-treesitter. 在 "CursorMoved" 时计算, 会影响速度.
   --- NOTE: 当 cursor 在注释上的时候不会显示 indentLine 颜色.
-  show_current_context = true,  -- VVI: 显示当前同一个 context 内的 indentLine 颜色.
+  use_treesitter = true,  -- NOTE: use treesitter when possible. 默认 false.
+  --use_treesitter_scope = true,  -- VVI: 不要设置, 会导致 indentline 显示不如预期.
+  show_current_context = true,  -- NOTE: 显示当前同一个 context 内的 indentLine 颜色.
   --show_current_context_start = true,  -- 在 indentLine 起始行添加 Underline.
   --show_current_context_start_on_current_line = true,  -- even when the cursor is on the same line.
 
