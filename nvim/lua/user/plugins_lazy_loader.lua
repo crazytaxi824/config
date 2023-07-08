691bd4f2abe660c6b13ce72af33323b3251a1344
crazytaxi <crazytaxi.git@outlook.com>
2023-07-09 01:20:19 +1000

    update(lazyload): plugins list

--- nvim/lua/user/plugins_lazy_loader.lua
+++ nvim/lua/user/plugins_lazy_loader.lua
@@ -1,20 +1,14 @@
---- lazy load opt plugins,
---- require('packer').loader("foo bar ..."), 名字可以查看 `plugin/packer_compiled.lua`
-
---- VVI: 加载顺序很重要
+--- lazy load opt plugins
 local lazyload_plugins = {
-  'nvim-treesitter',  -- TreeSitter, NOTE: 必须要 lazyload, 否则严重影响 neovim startup 速度.
+  'nvim-treesitter',  -- Tree Sitter, NOTE: 必须要 lazyload, 否则严重影响 neovim startup 速度.
   'indent-blankline.nvim',
 
-  --'nvim-tree.lua',  -- NOTE: 不推荐使用 lazyload, 会导致 `$ nvim dir` 直接打开文件夹的时候出现问题.
   'bufferline.nvim',  -- NOTE: 需要先设置 showtabline=2 (always show tabline), 否则在加载 bufferline 后屏幕会向下移动一行.
   'lualine.nvim',
 
+  'telescope.nvim',
   'toggleterm.nvim',
   'gitsigns.nvim',
-
-  --- VVI: 以下插件使用 lazyload 时, 需要设置 {bufread = false}. 否则会多次触发 FileType event.
-  'telescope.nvim',
 }
 
 return lazyload_plugins
