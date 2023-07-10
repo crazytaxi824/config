c8b0286d79a3b13b964816d03f54e3f53e7a4899
crazytaxi <crazytaxi.git@outlook.com>
2023-07-11 09:54:57 +1000

    upgrade(LuaSnip)

--- nvim/lua/user/plugins_loader.lua
+++ nvim/lua/user/plugins_loader.lua
@@ -184,7 +184,7 @@ local plugins = {
 
   --- snippet engine, for "cmp_luasnip", 每次打开文件都会有一个 [Scratch] buffer.
   {"L3MON4D3/LuaSnip",
-    commit = str_or_nil("a658ae2"),
+    commit = str_or_nil("45a4e89"),
     build = "make install_jsregexp",  -- https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#transformations
     config = function() require("user.plugin_settings.luasnip_snippest") end,
     dependencies = {"rafamadriz/friendly-snippets"},  -- snippets content
