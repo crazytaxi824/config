4cc304ae8c736683afd7f2a2388e6efd9bde72ac
crazytaxi <crazytaxi.git@outlook.com>
2023-07-19 14:13:00 +1000

    refactor(my_term): private functions

--- nvim/lua/user/my_term.lua
+++ nvim/lua/user/my_term.lua
@@ -4,12 +4,14 @@
 
 local name_tag=';#my_term#'
 
+local global_my_term_cache = {}
+
 local default_opts = {
   cmd = vim.go.shell,  --- 相当于 os.getenv('SHELL')
   startinsert = true,
   jobdone_exit = true,
   win_height = 16,
-  count = 1,  -- v:count1
+  id = 1,  -- v:count1, 尽量保证唯一性
 }
 
 local function find_exist_term_win()
@@ -29,104 +31,117 @@ local function find_exist_term_win()
   end
 end
 
-local function new_term_obj(opts)
-  opts = opts or {}
-  local term_obj = vim.tbl_deep_extend('force', default_opts, opts)
-
-  term_obj.__start_insert = function()
-    if term_obj.startinsert then
-      --- go back to terminal window for `:startinsert`
-      if vim.fn.win_gotoid(term_obj.win_id) == 1 then
-        vim.cmd('startinsert')
-      end
+--- private functions
+local function __start_insert(term_obj)
+  if term_obj.startinsert then
+    --- go back to terminal window for `:startinsert`
+    if term_obj.win_id and vim.fn.win_gotoid(term_obj.win_id) == 1 then
+      vim.cmd('startinsert')
     end
   end
-
-  --- 根据 term name_tag :bwipeout terminal buffer.
-  term_obj.__jobdone_autocmd = function()
-    vim.api.nvim_create_autocmd("TermClose", {
-      buffer = term_obj.bufnr,
-      callback = function(params)
-        if term_obj.jobdone_exit then
-          vim.cmd('bwipeout ' .. params.buf)
-        else
-          vim.cmd('stopinsert')
-        end
-      end
-    })
-  end
-
-  --- terminal open and exec command
-  term_obj.__exec_cmd = function()
-    if vim.fn.win_gotoid(term_obj.win_id) == 1 then
-      --- 使用 termopen() 开打 terminal
-      local cmd = term_obj.cmd .. name_tag  .. term_obj.count
-      term_obj.job_id = vim.fn.termopen(cmd)
-    else
-      vim.notify("win_id: " .. term_obj.win_id .. " is not exist", vim.log.levels.WARN)
-    end
-
-    --- TODO; 如果 count 相同则 bw 之前的 buffer. 先打开新的 term 再删除就的
-  end
-
-  --- NOTE: 创建一个 window 用于 terminal 运行.
-  term_obj.__create_new_term_win = function()
-    local exist_win_id = find_exist_term_win()
-
-    if exist_win_id and vim.fn.win_gotoid(exist_win_id) == 1 then
-      vim.cmd('vertical rightbelow new')  --- at least 1 terminal window exist
-    else
-      vim.cmd('horizontal botright ' .. term_obj.win_height .. 'new')  --- no terminal window exist
-    end
-
-    term_obj.win_id = vim.api.nvim_get_current_win()
-    term_obj.bufnr = vim.api.nvim_get_current_buf()
-
-    vim.bo[term_obj.bufnr].buflisted = false
-  end
-
-  term_obj.__reload_exist_term_buffer = function()
-    local exist_win_id = find_exist_term_win()
-
-    if exist_win_id and vim.fn.win_gotoid(exist_win_id) == 1 then
-      vim.cmd('vertical rightbelow sbuffer ' .. term_obj.bufnr)  --- at least 1 terminal window exist
-    else
-      vim.cmd('horizontal botright sbuffer' .. term_obj.bufnr .. ' | resize ' .. term_obj.win_height)  --- no terminal window exist
-    end
-
-    term_obj.win_id = vim.api.nvim_get_current_win()
-  end
-
-  return term_obj
 end
 
---- set quickfix list for terminal list.
-local function set_term_qf(win_id, opts)
-  local bufnr = vim.api.nvim_win_get_buf(win_id)
-  vim.fn.setqflist({{bufnr=bufnr, module="my_term" .. opts.count}}, 'a')
-  vim.cmd('vertical botright copen 20')  -- 最小值为 20
-  vim.fn.win_gotoid(win_id)  -- go back to terminal window for `:startinsert`
+--- 根据 term name_tag :bwipeout terminal buffer.
+local function __jobdone_autocmd(term_obj)
+  vim.api.nvim_create_autocmd("TermClose", {
+    buffer = term_obj.bufnr,
+    callback = function(params)
+      if term_obj.jobdone_exit then
+        vim.cmd('bwipeout ' .. params.buf)
+      else
+        vim.cmd('stopinsert')
+      end
+    end
+  })
 end
+
+--- terminal open and exec command
+local function __exec_cmd(term_obj)
+  if term_obj.win_id and vim.fn.win_gotoid(term_obj.win_id) == 1 then
+    --- 使用 termopen() 开打 terminal
+    local cmd = term_obj.cmd .. name_tag  .. term_obj.id
+    term_obj.job_id = vim.fn.termopen(cmd)
+  else
+    vim.notify("win_id: " .. term_obj.win_id .. " is not exist", vim.log.levels.WARN)
+  end
+
+  --- TODO; 如果 id 相同则 bw 之前的 buffer. 先打开新的 term 再删除就的
+end
+
+--- NOTE: 创建一个 window 用于 terminal 运行.
+local function __create_new_term_win(term_obj)
+  local exist_win_id = find_exist_term_win()
+
+  if vim.fn.win_gotoid(exist_win_id) == 1 then
+    vim.cmd('vertical rightbelow new')  --- at least 1 terminal window exist
+  else
+    vim.cmd('horizontal botright ' .. term_obj.win_height .. 'new')  --- no terminal window exist
+  end
+
+  term_obj.win_id = vim.api.nvim_get_current_win()
+  term_obj.bufnr = vim.api.nvim_get_current_buf()
+
+  vim.bo[term_obj.bufnr].buflisted = false
+end
+
+local function __reload_exist_term_buffer(term_obj)
+  local exist_win_id = find_exist_term_win()
+
+  if vim.fn.win_gotoid(exist_win_id) == 1 then
+    vim.cmd('vertical rightbelow sbuffer ' .. term_obj.bufnr)  --- at least 1 terminal window exist
+  else
+    vim.cmd('horizontal botright sbuffer' .. term_obj.bufnr .. ' | resize ' .. term_obj.win_height)  --- no terminal window exist
+  end
+
+  term_obj.win_id = vim.api.nvim_get_current_win()
+end
+
+local function __term_buf_exist(term_obj)
+  if term_obj.bufnr and vim.fn.bufexists(term_obj.bufnr) == 1 then
+    return true
+  end
+end
+
+--- set quickfix list for terminal list. --- {{{
+-- local function set_term_qf(win_id, opts)
+--   local bufnr = vim.api.nvim_win_get_buf(win_id)
+--   vim.fn.setqflist({{bufnr=bufnr, module="my_term" .. opts.id}}, 'a')
+--   vim.cmd('vertical botright copen 20')  -- 最小值为 20
+--   vim.fn.win_gotoid(win_id)  -- go back to terminal window for `:startinsert`
+-- end
+-- -- }}}
 
 --- return an term object
 function NewTerm(opts)
-  local my_term = new_term_obj(opts)
+  opts = opts or {}
+  local my_term = vim.tbl_deep_extend('force', default_opts, opts)
 
   my_term.run = function()
     --- first: open a window for terminal, get win_id.
-    my_term.__create_new_term_win()
+    if __term_buf_exist(my_term) then
+      --- TODO scrach buffer
+      -- local scratch_bufnr = vim.api.nvim_create_buf(false, true)
 
-    my_term.__exec_cmd()
+      vim.cmd('bw '..my_term.bufnr)
+    end
 
-    my_term.__jobdone_autocmd()
+    __create_new_term_win(my_term)
+
+    __exec_cmd(my_term)
+
+    __jobdone_autocmd(my_term)
 
     --- TODO: hooks on_open
 
-    my_term.__start_insert()  --- NOTE: after exec cmd, 单独执行避免过程中跳转到其他 window.
+    __start_insert(my_term)  --- NOTE: after exec cmd, 单独执行避免过程中跳转到其他 window.
   end
 
   my_term.open = function()
-    my_term.__reload_exist_term_buffer()
+    if __term_buf_exist(my_term) then
+      __reload_exist_term_buffer(my_term)
+    else
+      vim.notify('Terminal: "#' .. my_term.id .. '" is not Active', vim.log.levels.WARN)
+    end
   end
 
   return my_term
