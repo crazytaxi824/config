602fa2bc1eb1ea517bc2e9536858b8bf7efc663c
crazytaxi <crazytaxi.git@outlook.com>
2023-07-23 15:39:47 +1000

    update(keymap): change editor background and VertSplit color

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -82,9 +82,13 @@ local function toggle_background_color()
   if c.ctermbg == 234 then
     c.ctermbg = nil
     vim.api.nvim_set_hl(0, "Normal", c)
+    vim.api.nvim_set_hl(0, "NormalNC", {link="Normal"})
+    vim.api.nvim_set_hl(0, "VertSplit", {ctermfg=236})
   else
     c.ctermbg = 234
     vim.api.nvim_set_hl(0, "Normal", c)
+    vim.api.nvim_set_hl(0, "NormalNC", {ctermbg=235})
+    vim.api.nvim_set_hl(0, "VertSplit", {ctermfg=237, ctermbg=235})
   end
 end
 -- -- }}}
