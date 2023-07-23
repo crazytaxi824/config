3a872bcb64be53db6742737d2ad516889e60a4db
crazytaxi <crazytaxi.git@outlook.com>
2023-07-23 10:44:49 +1000

    chore: unused var

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -47,12 +47,10 @@ local key_fn = require('user.utils.keymaps')
 --- wipeout all terminals ------------------------------------------------------ {{{
 --- NOTE: close all terminals' window; stop all terminals' job; wipeout all terminals' buffer.
 local function wipeout_all_terminals()
-  local buf_list = {}
-
   -- 获取所有 bufnr, 判断 bufname 是否匹配 term://*
   for bufnr = vim.fn.bufnr('$'), 1, -1 do
     if vim.bo[bufnr].buftype == 'terminal' then
-      vim.api.nvim_buf_delete(bufnr, {force=true})
+      vim.api.nvim_buf_delete(bufnr, {force=true})  -- wipeout buffe
     end
   end
 end
