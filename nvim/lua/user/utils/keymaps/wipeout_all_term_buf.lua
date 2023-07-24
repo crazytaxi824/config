40fd763206e1ddb74d85f3dd4bea08dff96ca018
crazytaxi <crazytaxi.git@outlook.com>
2023-07-24 17:14:18 +1000

    fix: bufnr('$') cannot be used in for loop

--- nvim/lua/user/utils/keymaps/wipeout_all_term_buf.lua
+++ nvim/lua/user/utils/keymaps/wipeout_all_term_buf.lua
@@ -3,8 +3,7 @@
 local M = {}
 
 M.wipeout_all_terminals = function()
-  -- 获取所有 bufnr, 判断 bufname 是否匹配 term://*
-  for bufnr = vim.fn.bufnr('$'), 1, -1 do
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
     if vim.bo[bufnr].buftype == 'terminal' then
       vim.api.nvim_buf_delete(bufnr, {force=true})  -- wipeout buffe
     end
