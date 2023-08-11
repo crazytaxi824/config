aee610d5d527911724146614a1ba7bec7132e94a
crazytaxi <crazytaxi.git@outlook.com>
2023-08-11 10:12:40 +1000

    fix(fold): require('nvim-treesitter.parsers')

--- nvim/lua/user/fold/fold_treesitter.lua
+++ nvim/lua/user/fold/fold_treesitter.lua
@@ -1,11 +1,11 @@
-local nvim_ts_ok, nvim_ts_parsers = pcall(require, "nvim-treesitter.parsers")
-if not nvim_ts_ok then
-  return
-end
-
 local M = {}
 
 M.set_foldexpr = function(bufnr)
+  local nvim_ts_ok, nvim_ts_parsers = pcall(require, "nvim-treesitter.parsers")
+  if not nvim_ts_ok then
+    return
+  end
+
   --- treesitter 是否有对应的 parser.
   local has_parser = nvim_ts_parsers.has_parser(nvim_ts_parsers.get_buf_lang(bufnr))
   if not has_parser then
