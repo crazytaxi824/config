7347fe3745a24e97348287023ed915144e17fbe2
crazytaxi <crazytaxi.git@outlook.com>
2023-08-11 17:41:32 +1000

    docs

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,11 +3,6 @@
 -------------------------------------+----------------------------------+------------------------------------
 -- `:checkhealth`                    | (*) 检查 nvim 环境               | neovim
 -------------------------------------+----------------------------------+------------------------------------
--- `:PackerClean`                    | Remove disabled/unused plugins.  | "wbthomason/packer.nvim"
--- `:PackerCompile`                  | 每次修改 packer.startup() 设置都需要重新运行 PackerCompile 否则修改不生效.
--- `:PackerSync`                     | :PackerUpdate, Compile plugins.  | *
--- `:PackerSnapshot foo`             | 拍摄快照记录 plugins 版本        |
--------------------------------------+----------------------------------+------------------------------------
 -- `:LspInfo`                        | 所有已启动的 LSP 列表.           | "neovim/nvim-lspconfig"
 -------------------------------------+----------------------------------+------------------------------------
 -- `:Mason`                          | lsp servers 安装辅助工具         | "williamboman/mason.nvim"
@@ -24,11 +19,8 @@
 -- `:NullLsLog`                      | 查看 null-ls debug log           | "jose-elias-alvarez/null-ls.nvim"
 -- `:NullLsInfo`                     | 查看 null-ls 加载信息            |
 -------------------------------------+----------------------------------+------------------------------------
--- `:LuaCacheProfile`                | 查看文件加载时间                 | "lewis6991/impatient.nvim" 需要 enable_profile()
--- `:LuaCacheClear`                  | 清空缓存文件, 下次启动重新生成   |    清空 luacache_chunks, luacache_modpaths
--------------------------------------+----------------------------------+------------------------------------
--- `$ nvim --startuptime [logfile] [open_file]`  -- 将 nvim 打开 open_file 过程中的所有耗时打印到 logfile 中
--- eg: `$ nvim --startuptime log src/main.go`    -- 将 nvim 打开 open_file 过程中的所有耗时打印到 ./log 文件中
+-- `$ nvim --startuptime log src/main.go`  -- 将 nvim 打开 open_file 过程中的所有耗时打印到 ./log 文件中
+-- `$ nvim --clean src/main.go`   -- 不加载任何 plugins
 -------------------------------------+----------------------------------+------------------------------------
 
 --- for Debugging Neovim plugins. `:LspInfo`, `:LspLog`
@@ -41,7 +33,7 @@ __Debug_Neovim = {
 --- 读取设置: ~/.config/nvim/lua/user/xxx.lua
 require "user.core"     -- VVI: 必须放在最前面加载, 因为有全局函数需要被用到.
 require "user.colors"   -- VVI: 必须放在最前面加载, 因为有全局变量 "Color", 很多 plugins 需要用到.
-require "user.options" -- vimrc 设置
+require "user.options"  -- vimrc 设置
 require "user.fold"     -- fold-lsp -> fold-treesitter -> fold-indent
 require "user.lsp"      -- 加载 vim.lsp/vim.diagnostic 相关设置. 这里不是插件设置, 是内置参数设置.
                         -- user/lsp 是个文件夹, 这里是加载的 user/lsp/init.lua
