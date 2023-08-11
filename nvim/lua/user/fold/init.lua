0a970bf941f731403c60f975fb00415e011dedad
crazytaxi <crazytaxi.git@outlook.com>
2023-08-11 10:19:45 +1000

    refactor(fold)

--- nvim/lua/user/fold/init.lua
+++ nvim/lua/user/fold/init.lua
@@ -1,22 +1,20 @@
 local expr_lsp = require("user.fold.fold_lsp")
 local expr_ts = require("user.fold.fold_treesitter")
 
-local M = {}
-
 --- 使用 lsp 来 fold, 如果 lsp_fold 设置成功则返回 true.
-M.lsp_fold = function(client, bufnr)
+local function lsp_fold(client, bufnr)
   return expr_lsp.set_foldexpr(client, bufnr)
 end
 
 --- 使用 treesitter 来 fold, 如果 treesitter_fold 设置成功则返回 true.
-M.treesitter_fold = function(bufnr)
+local function treesitter_fold(bufnr)
   if expr_ts then
     return expr_ts.set_foldexpr(bufnr)
   end
 end
 
 --- foldmethod=indent
-M.indent_fold = function(bufnr)
+local function indent_fold(bufnr)
   vim.api.nvim_buf_call(bufnr, function()
     vim.opt_local.foldmethod = 'indent'
   end)
@@ -27,13 +25,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
   pattern = {"*"},
   callback = function(params)
     local client = vim.lsp.get_client_by_id(params.data.client_id)
-    if not M.lsp_fold(client, params.buf)  -- try lsp_fold
-      and not M.treesitter_fold(params.buf) -- try treesitter_fold
+    if not lsp_fold(client, params.buf)  -- try lsp_fold
+      and not treesitter_fold(params.buf) -- try treesitter_fold
     then
-      M.indent_fold(params.buf)  -- fallback to fold-indent
+      indent_fold(params.buf)  -- fallback to fold-indent
     end
   end,
   desc = "set fold when LspAttach"
 })
 
-return M
+
+
