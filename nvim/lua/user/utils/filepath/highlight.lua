586b76d8c066800064cda7a7e2f7a2d6ab21c194
crazytaxi <crazytaxi.git@outlook.com>
2023-08-19 17:57:55 +1000

    chore(filepath): remove hl_url highlight group

--- nvim/lua/user/utils/filepath/highlight.lua
+++ nvim/lua/user/utils/filepath/highlight.lua
@@ -5,10 +5,7 @@ local pat = require('user.utils.filepath.pattern')
 local M = {}
 
 local hl_filepath = 'my_filepath'
-local hl_url = 'my_url'
-
 vim.api.nvim_set_hl(0, hl_filepath, {underline = true}) -- 自定义颜色, for matchadd()
-vim.api.nvim_set_hl(0, hl_url, {ctermfg = Color.blue, underline = true}) -- 自定义颜色, for matchadd()
 
 --- NOTE: matchadd() 每次执行只能作用在 current window 上. 所有在该 window 打开的 buffer 都会收到影响.
 --- 而且状态持续, 当该 window 打开别的 buffer 时, highlight 一样会存在.
@@ -21,7 +18,7 @@ M.highlight_filepath = function(win_id, priority)
   --- 如果该 window 已经设置了 hl_filepath or hl_url 则 return
   local matches = vim.fn.getmatches(win_id)
   for _, m in ipairs(matches) do
-    if m.group == hl_filepath or m.group == hl_url then
+    if m.group == hl_filepath then
       return
     end
   end
@@ -31,7 +28,7 @@ M.highlight_filepath = function(win_id, priority)
   --- matchadd() 默认的 highlight priority 是 10. 这时 Search 的 highlight 会被 matchadd() 覆盖.
   vim.fn.matchadd(hl_filepath, pat.file_schema_pattern, priority, -1, { window = win_id })
   vim.fn.matchadd(hl_filepath, pat.filepath_pattern,    priority, -1, { window = win_id })
-  vim.fn.matchadd(hl_url,      pat.url_schema_pattern,  priority, -1, { window = win_id })
+  vim.fn.matchadd(hl_filepath, pat.url_schema_pattern,  priority, -1, { window = win_id })
 end
 
 --- 删除自定义 filepath highlight.
@@ -43,7 +40,7 @@ M.highlight_filepath_clear = function(win_id)
 
   local matches = vim.fn.getmatches(win_id)
   for _, m in ipairs(matches) do
-    if m.group == hl_filepath or m.group == hl_url then
+    if m.group == hl_filepath then
       vim.fn.matchdelete(m.id, win_id)
     end
   end
