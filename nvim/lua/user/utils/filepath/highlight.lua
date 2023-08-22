8ec8fe07d0299ef1ed98940e1914f1c113f865dc
crazytaxi <crazytaxi.git@outlook.com>
2023-08-22 19:47:00 +1000

    refactor(filepath)!: setup() -> highlight() & jump_to_file()

--- nvim/lua/user/utils/filepath/highlight.lua
+++ nvim/lua/user/utils/filepath/highlight.lua
@@ -1,49 +1,26 @@
---- 通过 matchadd() 来 highlight filepath 和 URL
-
-local pat = require('user.utils.filepath.pattern')
+local parse = require('user.utils.filepath.parse')
 
 local M = {}
 
-local hl_filepath = 'my_filepath'
-vim.api.nvim_set_hl(0, hl_filepath, {underline = true}) -- 自定义颜色, for matchadd()
+local cache_hl = nil
 
---- NOTE: matchadd() 每次执行只能作用在 current window 上. 所有在该 window 打开的 buffer 都会收到影响.
---- 而且状态持续, 当该 window 打开别的 buffer 时, highlight 一样会存在.
-M.highlight_filepath = function(win_id, priority)
-  --- win_id 不存在
-  if not vim.api.nvim_win_is_valid(win_id) then
+-- --- NOTE: matchadd() 每次执行只能作用在 current window 上. 所有在该 window 打开的 buffer 都会收到影响.
+-- --- 而且状态持续, 当该 window 打开别的 buffer 时, highlight 一样会存在.
+M.highlight_filepath = function()
+  --- delete previous highlight
+  if cache_hl and vim.api.nvim_buf_is_valid(cache_hl.bufnr) then
+    vim.api.nvim_buf_clear_namespace(cache_hl.bufnr, cache_hl.ns_id, 0, -1)
+  end
+  cache_hl = nil -- delete cache
+
+  local r = parse.parse(vim.fn.expand('<cWORD>'), 'hl')
+  if not r then
     return
   end
 
-  --- 如果该 window 已经设置了 hl_filepath or hl_url 则 return
-  local matches = vim.fn.getmatches(win_id)
-  for _, m in ipairs(matches) do
-    if m.group == hl_filepath then
-      return
-    end
-  end
-
-  priority = priority or 0
-  --- highlight filepath
-  --- matchadd() 默认的 highlight priority 是 10. 这时 Search 的 highlight 会被 matchadd() 覆盖.
-  vim.fn.matchadd(hl_filepath, pat.file_schema_pattern, priority, -1, { window = win_id })
-  vim.fn.matchadd(hl_filepath, pat.filepath_pattern,    priority, -1, { window = win_id })
-  vim.fn.matchadd(hl_filepath, pat.url_schema_pattern,  priority, -1, { window = win_id })
-end
-
---- 删除自定义 filepath highlight.
-M.highlight_filepath_clear = function(win_id)
-  --- win_id 不存在
-  if not vim.api.nvim_win_is_valid(win_id) then
-    return
-  end
-
-  local matches = vim.fn.getmatches(win_id)
-  for _, m in ipairs(matches) do
-    if m.group == hl_filepath then
-      vim.fn.matchdelete(m.id, win_id)
-    end
-  end
+  --- highlight current filepath
+  local ns_id = vim.api.nvim_buf_add_highlight(r.bufnr, 0, "Underlined", r.hl_lnum, r.hl_start_col, r.hl_end_col)
+  cache_hl = vim.tbl_deep_extend("force", r, {ns_id = ns_id})
 end
 
 return M
--- nvim/lua/user/utils/filepath/init.lua
+++ nvim/lua/user/utils/filepath/init.lua
@@ -2,28 +2,24 @@ local fp_hl = require('user.utils.filepath.highlight')
 local fp_jump = require('user.utils.filepath.jump_to_file')
 
 local M = {
-  highlight = fp_hl.highlight_filepath,
-  highlight_clear = fp_hl.highlight_filepath_clear,
-
+  highlight = fp_hl.highlight,
   n_jump = fp_jump.n_jump_cWORD,
   v_jump = fp_jump.v_jump_selected,
   v_system_open = fp_jump.v_system_open_selected,
 }
 
---- 在 terminal 和 dap window 中显示 filepath highlight.
-vim.api.nvim_create_autocmd('BufWinEnter', {
-  pattern = {"*"},
-  callback = function(params)
-    local curr_win_id = vim.api.nvim_get_current_win()
-
-    --- terminal and dap
-    if vim.api.nvim_buf_is_valid(params.buf) and (vim.bo[params.buf].buftype == 'terminal' or vim.bo[params.buf].filetype == 'dap-repl') then
-      fp_hl.highlight_filepath(curr_win_id)
-    else
-      fp_hl.highlight_filepath_clear(curr_win_id)
+M.setup = function(bufnr)
+  --- autocmd highlight filepath under cursor
+  vim.api.nvim_create_autocmd({"CursorHold"}, {
+    buffer = bufnr,
+    callback = function(params)
+      fp_hl.highlight_filepath()
     end
-  end,
-  desc = "terminal: filepath highlight",
-})
+  })
+
+  --- set keymap jump to file/dir
+  local opts = { buffer = bufnr, noremap = true, silent = true, desc = "filepath: Jump to file" }
+  vim.keymap.set('n', '<S-CR>', function() fp_jump.n_jump_cWORD() end, opts)
+end
 
 return M
