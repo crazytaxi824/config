404e9c9ccb394479e306b9e9e13d0112376c81ba
crazytaxi <crazytaxi.git@outlook.com>
2023-08-26 17:02:35 +1000

    chore(lspconfig): move lsp list to another file

--- nvim/lua/user/lsp/lsp_config/init.lua
+++ nvim/lua/user/lsp/lsp_config/init.lua
@@ -12,60 +12,8 @@ end
 require('lspconfig.ui.windows').default_options.border = {"▄","▄","▄","█","▀","▀","▀","█"}
 vim.cmd('hi! link LspInfoBorder FloatBorder')
 
---- 以下命令行工具可以通过 mason.nvim 安装, 也可以通过 brew 安装到 $PATH 中.
---- filetypes: 可以通过 `:LspInfo` 查看. 用于 autocmd.
---- lspconfig_name: filetype
-local lsp_servers_map = {
-  lua_ls = {
-    cmd = "lua-language-server",
-    mason = "lua-language-server",
-    filetypes = {'lua'}
-  },
-  tsserver = {
-    cmd = "typescript-language-server",
-    mason = "typescript-language-server",
-    filetypes = {
-      'javascript', 'javascriptreact', 'javascript.jsx',
-      'typescript', 'typescriptreact', 'typescript.tsx',
-    }
-  },
-  bashls = {
-    cmd = "bash-language-server",
-    mason = "bash-language-server",
-    filetypes = {'sh'}
-  },
-  gopls = {
-    cmd = "gopls",
-    mason = "gopls",
-    install = "go install golang.org/x/tools/gopls@latest",
-    filetypes = {'go', 'gomod', 'gowork', 'gotmpl'}
-  },
-  pyright = {
-    cmd = "pyright",
-    mason = "pyright",
-    filetypes = {'python'}
-  },
-  html = {
-    cmd = "vscode-html-language-server",
-    mason = "html-lsp",
-    filetypes = {'html'}
-  },
-  cssls = {
-    cmd = "vscode-css-language-server",
-    mason = "css-lsp",
-    filetypes = {'css', 'scss', 'less'}
-  },
-  jsonls = {
-    cmd = "vscode-json-language-server",
-    mason = "json-lsp",
-    filetypes = {'json', 'jsonc'}
-  },
-  bufls = {  -- protobuf lsp
-    cmd = "bufls",
-    mason = "buf-language-server",
-    filetypes = {'proto'},
-  }
-}
+--- 需要设置的 lsp 列表.
+local lsp_servers_map = require('user.lsp.lsp_config.lsp_list')
 
 local function lspconfig_setup(lsp_svr)
   --- opts 必须包含 on_attach, capabilities 两个属性.
