ca6d439d40f4cf65cb61e895feb882ac6c833906
crazytaxi <crazytaxi.git@outlook.com>
2024-04-09 17:26:22 +1000

    docs: new backup formatter plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -358,8 +358,8 @@ local plugins = {
 
   --- recommanded plugins ------------------------------------------------------ {{{
   --- null-ls 替代:
-  --{"mfussenegger/nvim-lint"},     -- linter
-  --{"mhartington/formatter.nvim"}  -- formatter
+  --{"mfussenegger/nvim-lint"}, -- linter
+  --{"stevearc/conform.nvim"},  -- formatter
 
   --{"kyazdani42/nvim-web-devicons"}, -- Nerd Fonts 提供 icons 需要 patch 字体
   --{"nvim-neo-tree/neo-tree.nvim"},  -- File explorer. nvim-tree.lua 替代
