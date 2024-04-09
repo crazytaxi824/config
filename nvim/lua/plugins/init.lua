bc823011108ecc24e4ee4a90881f76753abc2018
crazytaxi <crazytaxi.git@outlook.com>
2024-04-09 12:12:53 +1000

    upgrade: 09-04-2024

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -81,7 +81,7 @@ local plugins = {
   --- By convention, if you want to write a query, use the `queries/` directory,
   --- but if you want to extend a query use the `after/queries/` directory.
   {"nvim-treesitter/nvim-treesitter",
-    commit = "11a3584",  -- NOTE: tag 更新太慢, 建议两周更新一次.
+    commit = "46b587e",  -- NOTE: tag 更新太慢, 建议两周更新一次.
     --build = ":TSUpdate",  -- NOTE: 推荐手动执行, 批量自动安装 parser 容易卡死.
     config = function() require("plugins.settings.treesitter") end,
     dependencies = {
