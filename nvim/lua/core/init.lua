78ae3df5f9b6b3a1c5b41d7cccd032e4218cf37f
crazytaxi <crazytaxi.git@outlook.com>
2024-04-10 09:27:02 +1000

    docs(core)

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -5,6 +5,8 @@ require("core.funcs.notify")    -- Notify() 函数
 require "core.colors"   -- VVI: 必须放在最前面加载, 因为有全局变量 "Color", 很多 plugins 需要用到.
 require "core.options"  -- vimrc 设置
 require "core.keymaps"  -- keymap 设置
+
+--- 以下是 autocmd 设置相关 options
 require "core.terminal" -- terminal buffer 自动设置 nonumber signcolumn ...
 require "core.wrap"     -- 根据 set wrap 设置 cursor move.
 
