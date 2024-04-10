eca99ebdbc5dd94bdf95885896bc2a4cc0282a17
crazytaxi <crazytaxi.git@outlook.com>
2024-04-10 12:14:29 +1000

    update(fold): change require core.fold position

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -2,13 +2,14 @@
 require("core.funcs.fzf_edit")  -- 用于编辑 fzf multi selected item
 require("core.funcs.notify")    -- Notify() 函数
 
-require "core.colors"   -- VVI: 必须放在最前面加载, 因为有全局变量 "Color", 很多 plugins 需要用到.
-require "core.options"  -- vimrc 设置
-require "core.keymaps"  -- keymap 设置
+require("core.colors")   -- VVI: 必须放在最前面加载, 因为有全局变量 "Color", 很多 plugins 需要用到.
+require("core.options")  -- vimrc 设置
+require("core.keymaps")  -- keymap 设置
 
---- 以下是 autocmd 设置相关 options
-require "core.terminal" -- terminal buffer 自动设置 nonumber signcolumn ...
-require "core.wrap"     -- 根据 set wrap 设置 cursor move.
+--- VVI: 以下使用 autocmd 设置相关 options, 需要放在 "core.options" 后加载.
+require("core.fold")     -- lsp-fold & treesitter-fold autocmd
+require("core.terminal") -- terminal buffer 自动设置 nonumber signcolumn ...
+require("core.wrap")     -- 根据 set wrap 设置 cursor move.
 
 
 
