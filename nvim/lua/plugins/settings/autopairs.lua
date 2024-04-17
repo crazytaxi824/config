6cb023426a7c5de01fe9e11258b58a8ea0706d65
crazytaxi <crazytaxi.git@outlook.com>
2024-04-17 19:31:50 +1000

    docs(autopairs)

--- nvim/lua/plugins/settings/autopairs.lua
+++ nvim/lua/plugins/settings/autopairs.lua
@@ -55,8 +55,8 @@ local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
   return
 end
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({map_char={tex=""}}))
+local autopairs_cmp = require("nvim-autopairs.completion.cmp")
+cmp.event:on("confirm_done", autopairs_cmp.on_confirm_done({map_char={tex=""}}))
 
 
 
