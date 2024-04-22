d16e054809cf833ade15c6d0ed2d903a5d42849f
crazytaxi <crazytaxi.git@outlook.com>
2024-04-22 17:52:50 +1000

    update(treesitter): js ts highlights.scm

--- nvim/after/queries/javascript/highlights.scm
+++ nvim/after/queries/javascript/highlights.scm
@@ -2,5 +2,7 @@
 ;;; `:help treesitter-predicates`
 
 ;;; 定义 class Foo {...} 后, const foo = new Foo() 作为一个 type 而不是 constructor.
-(variable_declarator value: ((new_expression constructor: ((identifier) @type ))))
+(variable_declarator value: (new_expression constructor: ((identifier) @type )))
+
+;;; TODO: const foo = "bar" 定义为 @constant 而不是 @variable
 
