3f74e42c469dac901558706e547deebe5e218ba7
crazytaxi <crazytaxi.git@outlook.com>
2024-04-22 17:39:42 +1000

    feat(javascript): set @type to class type

---
+++ nvim/after/queries/javascript/highlights.scm
@@ -0,0 +1,6 @@
+; extends
+;;; `:help treesitter-predicates`
+
+;;; 定义 class Foo {...} 后, const foo = new Foo() 作为一个 type 而不是 constructor.
+(variable_declarator value: ((new_expression constructor: ((identifier) @type ))))
+
--- nvim/after/queries/typescript/highlights.scm
+++ nvim/after/queries/typescript/highlights.scm
@@ -3,6 +3,5 @@
 
 ;;; 将 "literal_type" 中的 null | undefined 显示为 @type
 ;;; 其他地方的 null | undefined 还是显示为 @constant.builtin | @variable.builtin
-(literal_type
-  [(null) (undefined)] @type)
+(literal_type [(null) (undefined)] @type)
 
