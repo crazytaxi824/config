585274112f0a3aebbb9fc3a3f98fdd8d8c766e35
crazytaxi <crazytaxi.git@outlook.com>
2024-04-26 12:53:41 +1000

    chore: comments color function

--- nvim/lua/utils/keymaps/toggle_comments_color.lua
+++ nvim/lua/utils/keymaps/toggle_comments_color.lua
@@ -1,23 +1,27 @@
 local M = {}
 
+local dim_color = { ctermfg=238 }
+
 local comment_hl_groups = {
+  "Comment",
+  "Todo",
+
   "@comment.todo",
   "@comment.note",
   "@comment.warning",
   "@comment.error",
+
   "@string.special.url.comment"
 }
 
 M.toggle_comment_color = function()
-  local c = vim.api.nvim_get_hl(0, {name="Comment"})
+  local comm = vim.api.nvim_get_hl(0, {name="Comment"})
 
-  if c.ctermfg == Color.comment_green then
-    vim.api.nvim_set_hl(0, "Comment", {ctermfg = 238})
+  if comm.ctermfg == Highlights["Comment"].ctermfg then
     for _, hl in ipairs(comment_hl_groups) do
-      vim.api.nvim_set_hl(0, hl, {}) -- {} 意思是 clear highlight
+      vim.api.nvim_set_hl(0, hl, dim_color)
     end
   else
-    vim.api.nvim_set_hl(0, "Comment", Highlights["Comment"])
     for _, hl in ipairs(comment_hl_groups) do
       vim.api.nvim_set_hl(0, hl, Highlights[hl])
     end
