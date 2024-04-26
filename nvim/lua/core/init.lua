76e90a60913f0ba829aadc393c1c6484dc487d8e
crazytaxi <crazytaxi.git@outlook.com>
2024-04-26 18:42:45 +1000

    chore(core): change require order

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -2,9 +2,9 @@
 require("core.funcs.fzf_edit")  -- 用于编辑 fzf multi selected item
 require("core.funcs.notify")    -- Notify() 函数
 
-require("core.colors")   -- VVI: 必须放在最前面加载, 因为有全局变量 "Color", 很多 plugins 需要用到.
-require("core.nerd_icons") -- nerd fonts icons
 require("core.options")  -- vimrc 设置
+require("core.colors")   -- VVI: 必须放在前面加载, 因为有全局变量 "Color", 很多 plugins 需要用到.
+require("core.nerd_icons") -- nerd fonts icons
 require("core.keymaps")  -- keymap 设置
 
 --- VVI: 以下使用 autocmd 设置相关 options, 需要放在 "core.options" 后加载.
