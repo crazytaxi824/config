5f29517a71a37dec22ce44a24cd41f3872566c1c
crazytaxi <crazytaxi.git@outlook.com>
2024-05-07 20:57:55 +1000

    docs

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -1,9 +1,9 @@
 --- VVI: 以下需要事先加载.
 require("core.funcs.fzf_edit")  -- 用于编辑 fzf multi selected item
 require("core.funcs.notify")    -- Notify() 函数
-
 require("core.options")  -- vimrc 设置
-require("core.colors")   -- VVI: 必须放在前面加载, 因为有全局变量 "Color", 很多 plugins 需要用到.
+
+require("core.colors")   -- 有全局变量 "Colors", 很多 plugins 需要用到.
 require("core.nerd_icons") -- nerd fonts icons
 require("core.keymaps")  -- keymap 设置
 
