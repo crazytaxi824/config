37e487fa3309f9aae9d6bddce63f0bf2c2b884bc
crazytaxi <crazytaxi.git@outlook.com>
2024-05-17 12:01:06 +1000

    upgrade: 17-05-2024

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -85,7 +85,7 @@ local plugins = {
   --- but if you want to extend a query use the `after/queries/` directory.
   {
     "nvim-treesitter/nvim-treesitter",
-    commit = "aa0c7dd",  -- NOTE: tag 更新太慢, 建议两周更新一次.
+    commit = "8012b55",  -- NOTE: tag 更新太慢, 建议两周更新一次.
     --build = ":TSUpdate",  -- NOTE: 推荐手动执行, 批量自动安装 parser 容易卡死.
     config = function() require("plugins.settings.treesitter") end,
     dependencies = {
@@ -100,7 +100,7 @@ local plugins = {
   --- 第一方 module 插件 ---
   {
     "nvim-treesitter/nvim-treesitter-context",  -- 顶部显示 cursor 所在 function 的定义.
-    commit = "7068ad6",
+    commit = "6d07641",
     config = function() require("plugins.settings.treesitter_ctx") end,
 
     lazy = true,  -- nvim-treesitter 加载时自动加载.
@@ -213,7 +213,7 @@ local plugins = {
   --- snippets content, 自定义 snippets 可以借鉴这个结构.
   {
     "rafamadriz/friendly-snippets",
-    commit = "3e9a3f5",
+    commit = "dd2fd12",
 
     lazy = true,  -- LuaSnip 加载时自动加载.
   },
@@ -236,7 +236,7 @@ local plugins = {
   --- 官方 LSP 引擎.
   {
     "neovim/nvim-lspconfig",
-    commit = "a27179f",
+    commit = "cee94b2",
     config = function() require("lsp.lsp_config") end,  -- NOTE: 如果加载地址为文件夹, 则会寻找文件夹中的 init.lua 文件.
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",  -- lsp 提供的代码补全. NOTE: lspconfig 必须在 cmp_nvim_lsp 之后加载, 否则可能无法提供代码补全.
@@ -256,7 +256,7 @@ local plugins = {
 
   {
     "stevearc/conform.nvim",
-    commit = "59d0dd2",
+    tag = "v5.7.0",
     config = function() require("plugins.settings.formatter_confrom") end,
 
     event = "VeryLazy",
@@ -306,7 +306,7 @@ local plugins = {
   {
     "mfussenegger/nvim-dap",  -- core debug tool
     -- tag = "0.7.0",
-    commit = "559d0bb",
+    commit = "5a2f712",
     config = function() require("plugins.settings.dap_debug") end,
 
     cmd = {'DapToggleBreakpoint', 'DapContinue', 'DapLoadLaunchJSON'},
