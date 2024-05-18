576b677a6d592d1b43699544364ee7d40d45eea4
crazytaxi <crazytaxi.git@outlook.com>
2024-05-18 15:53:59 +1000

    update(lsp-handler)

--- nvim/lua/lsp/_user_handlers.lua
+++ nvim/lua/lsp/_user_handlers.lua
@@ -12,12 +12,22 @@
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.handlers["textDocument/publishDiagnostics"], {
     --- `:help vim.diagnostic.Opts`
-    --- VVI: 必须为 true, 否则 diagnostic 无法正确显示颜色.
-    underline = true,  -- Enable underline, use default values.
-
     --- 使用 virtual_text 显示 diagnostic message.
     virtual_text = false,
-    --virtual_text = { spacing = 4 },  -- Enable virtual text, override spacing to 4
+    -- virtual_text = {
+    --   spacing = 4,  -- Enable virtual text, override spacing to 4
+    --   prefix = function(diag, index, total)
+    --     if diag.severity == vim.diagnostic.severity.ERROR then
+    --       return Nerd_icons.diag.error
+    --     elseif diag.severity == vim.diagnostic.severity.WARN then
+    --       return Nerd_icons.diag.warn
+    --     elseif diag.severity == vim.diagnostic.severity.INFO then
+    --       return Nerd_icons.diag.info
+    --     elseif diag.severity == vim.diagnostic.severity.HINT then
+    --       return Nerd_icons.diag.hint
+    --     end
+    --   end,
+    -- },
 
     --- 是否显示 diagnostic signs.
     signs = true,
@@ -39,7 +49,7 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
     --- `:help nvim_open_win()`
     focusable = false,  -- false: 重复执行 vim.lsp.buf.hover() 时不会进入 floating window.
     border = Nerd_icons.border,
-    anchor_bias = 'above',
+    anchor_bias = 'above',  -- popup window 优先向上弹出
 
     --- events, to trigger close floating window
     --- https://github.com/neovim/neovim/blob/master/runtime/lua/vim/lsp/util.lua
