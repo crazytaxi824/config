3bcd2dbeab5223bb3c72ed0b738571ba3c5ce031
crazytaxi <crazytaxi.git@outlook.com>
2024-05-25 19:43:41 +1000

    remove: lazygit

--- nvim/lua/misc/init.lua
+++ nvim/lua/misc/init.lua
@@ -2,7 +2,6 @@ require("misc.debug_autocmd")
 require("misc.debug_functions")
 require("misc.log_cotent_filter")
 require("misc.del_marks")
-require("misc.lazygit")
 
 require("misc.get_ts_info")
 require("misc.health")   -- 在 :checkhealth 时执行.
