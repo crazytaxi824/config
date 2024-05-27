b998f6dfff98639a5f9184196a6224e751e26b53
crazytaxi <crazytaxi.git@outlook.com>
2024-05-27 13:22:20 +1000

    fix(lua_ls): vim.uv defination

--- nvim/lua/lsp/plugins/lsp_config/langs/lua_ls.lua
+++ nvim/lua/lsp/plugins/lsp_config/langs/lua_ls.lua
@@ -9,16 +9,15 @@ return {
         globals = { "vim" },
       },
       workspace = {
+        checkThirdParty = false,
         --- 所有 runtimepath 文件夹.
         -- library = vim.api.nvim_get_runtime_file("", true),  -- NOTE: this is a lot slower
         library = {
           --- NOTE: `:help $VIMRUNTIME` 是 vim 内置的 runtime 环境变量. 和 `:set runtimepath?` 不同.
           vim.env.VIMRUNTIME,  -- /usr/local/Cellar/neovim/0.xxx/share/nvim/runtime/
-          vim.fn.stdpath("config"),  -- ~/.config/nvim/, NOTE: 包含以下所有路径.
-          -- vim.fn.stdpath("config") .. "/init.lua",  -- ~/.config/nvim/init.lua
-          -- vim.fn.stdpath("config") .. "/lua",     -- ~/.config/nvim/lua/
-          -- vim.fn.stdpath("config") .. "/after",   -- ~/.config/nvim/after/
-          --vim.fn.stdpath("config") .. "/plugin",  -- ~/.config/nvim/plugin/
+          vim.fn.stdpath("config"),  -- ~/.config/nvim/ 目录下的 global function
+          "${3rd}/luv/library",  -- vim.vu
+          -- "${3rd}/busted/library",
         },
       },
     },
