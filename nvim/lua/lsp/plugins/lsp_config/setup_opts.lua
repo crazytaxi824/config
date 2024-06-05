4ecc7368cc73ffecce319ab74eb0fdff6c2bf2e5
crazytaxi <crazytaxi.git@outlook.com>
2024-06-05 14:27:46 +1000

    fix(lsp): client.config.settings is empty

--- nvim/lua/lsp/plugins/lsp_config/setup_opts.lua
+++ nvim/lua/lsp/plugins/lsp_config/setup_opts.lua
@@ -71,7 +71,11 @@ M.on_init = function(client, result)
   if proj_local_settings.content[M.local_lspconfig_key] and proj_local_settings.content[M.local_lspconfig_key][client.name] then
     local local_settings = proj_local_settings.content[M.local_lspconfig_key][client.name]
     for key, value in pairs(local_settings) do
-      client.config.settings[key] = vim.tbl_deep_extend('force', client.config.settings[key], value)
+      if not client.config.settings[key] then
+        client.config.settings[key] = value
+      else
+        client.config.settings[key] = vim.tbl_deep_extend('force', client.config.settings[key], value)
+      end
     end
   end
 
