a09997f4539eabe90bb874197ef918b2a30b968e
crazytaxi <crazytaxi.git@outlook.com>
2024-12-16 08:15:22 +1100

    upgrade: 16-12-2024

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -54,7 +54,7 @@ local plugins = {
   {
     "folke/which-key.nvim",
     -- commit = "68e37e1",
-    tag = "v3.14.1",
+    tag = "v3.15.0",
     config = function() require("plugins.settings.which_key") end,
 
     event = "VeryLazy",
