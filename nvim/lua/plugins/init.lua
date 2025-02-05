439b343ff99d20873230b6b525a60e4c40bc496e
crazytaxi <crazytaxi.git@outlook.com>
2025-02-05 22:25:58 +1100

    breaking(gitsign)

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -315,7 +315,7 @@ local plugins = {
   --- https://github.com/lewis6991/gitsigns.nvim#troublenvim
   {
     "lewis6991/gitsigns.nvim",
-    commit = "817bd84",
+    commit = "9b36d49",
     -- tag = "v0.9.0",
     config = function() require("plugins.settings.git_signs") end,
 
