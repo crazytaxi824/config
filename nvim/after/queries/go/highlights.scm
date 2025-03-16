954410af4bf22a571dedac43b489a63f9f2ba2c2
crazytaxi <crazytaxi.git@outlook.com>
2025-03-16 21:40:44 +1100

    docs(treesitter): go

--- nvim/after/queries/go/highlights.scm
+++ nvim/after/queries/go/highlights.scm
@@ -1,7 +1,7 @@
 ; extends
 ;;; `:help treesitter-predicates`
 
-;;; 给 go struct 添加 private field 属性和颜色, eg: type Foo struct { bar string } 中的 bar (第一个字母是 lower case).
+;;; 给 struct{} 添加 private field 属性和颜色, eg: type Foo struct { bar string } 中的 bar (第一个字母是 lower case).
 ;;; 可以通过 `:InspectTree` 查看 node 属性, 得到 field_identifier | field_declaration ...
 ;;; 如果 "field_declaration" 中的 "name: field_identifier" 名字不是 Capital letter 开头, 则定义为 @property.private,
 ;;; NOTE: eg: type T struct { A, a, _a, 你好 string }, 其中 a, _a, 你好 都是私有属性, 无法在 package 外使用.
@@ -10,7 +10,7 @@
   (#lua-match? @property.private "^[^A-Z]")
   (#set! "priority" 200)))
 
-;;; 初始化 struct 时 private field 的颜色. p := P{a:1, b:2}
+;;; 申明 struct{} 时 private field 的颜色. p := P{a:1, b:2}
 ; (keyed_element (literal_element (identifier) @property.private
 ;   (#lua-match? @property.private "^[^A-Z]")))
 
