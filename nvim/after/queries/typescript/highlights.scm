24f22b97bc35fe648c9216d0bf9554b53eaae957
crazytaxi <crazytaxi.git@outlook.com>
2025-03-16 23:49:43 +1100

    update(treesitter): ts <null | undefined> union_type color

--- nvim/after/queries/typescript/highlights.scm
+++ nvim/after/queries/typescript/highlights.scm
@@ -3,7 +3,7 @@
 
 ;;; 将 "literal_type" 中的 null | undefined 显示为 @type
 ;;; 其他地方的 null | undefined 还是显示为 @constant.builtin | @variable.builtin
-(type_alias_declaration value: (union_type (literal_type [(null) (undefined)] @type)))
+((union_type (literal_type [(null) (undefined)] @type)))
 
 ;;; 定义 class Foo {...} 后, const foo = new Foo() 作为一个 type 而不是 constructor.
 ;(variable_declarator value: (new_expression constructor: ((identifier) @type )))
