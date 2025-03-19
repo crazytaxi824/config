c2892f4bd06c70f16dc3ef4da31cb81f399a4050
crazytaxi <crazytaxi.git@outlook.com>
2025-03-19 14:30:08 +1100

    add(lsp): ruff settings

---
+++ nvim/lua/lsp/plugins/lsp_config/langs/ruff.lua
@@ -0,0 +1,23 @@
+--- 主要是设置 lsp root_dir
+return {
+  root_dir = function(fname)
+    local root = vim.fs.root(0, {'.venv', 'pyproject.toml', 'ruff.toml', '.ruff.toml'})
+    if root then
+      --- 如果找到 root 则返回 root
+      return root
+    end
+
+    Notify(
+      {
+        "'.venv' NOT found in current or any parent directory.",
+        "Please run:",
+        "  'python3 -m venv .venv'",
+        "  'pip3 install -U debugpy'"
+      },
+      "WARN",
+      {title={"LSP", "gopls.lua"}, timeout = false}
+    )
+    return vim.uv.cwd()
+  end
+}
+
