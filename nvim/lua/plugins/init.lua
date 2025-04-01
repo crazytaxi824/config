a1fec9b16a2fac7c9d606111200ffd2a175abc03
crazytaxi <crazytaxi.git@outlook.com>
2025-04-01 14:22:18 +1100

    reverse(nvim-cmp)

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -106,7 +106,7 @@ local plugins = {
   --- Auto Completion ------------------------------------------------------------------------------
   {
     "hrsh7th/nvim-cmp",
-    commit = "1e1900b",
+    commit = "c273707",
     config = function() require("plugins.settings.cmp_completion") end,
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",  -- lsp 提供的代码补全
