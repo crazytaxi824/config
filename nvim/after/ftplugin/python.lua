d1fd0f5b919ca3d91dc06b3782817c6d46deca45
crazytaxi <crazytaxi.git@outlook.com>
2025-04-16 15:44:09 +1000

    update(python)

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -2,31 +2,34 @@
 --- check virtual environment
 local function venv()
   --- 项目环境
-  local root = vim.fs.root(0, '.venv')
-  if not root then
+  local py_venv = vim.fs.root(0, '.venv')
+  if not py_venv then
     return
   end
 
-  local local_venv = vim.fs.joinpath(root, '.venv/bin/activate')
-  if vim.uv.fs_stat(local_venv) then
-    return local_venv
+  if vim.uv.fs_stat(vim.fs.joinpath(py_venv, '.venv/bin/activate')) then
+    return py_venv
   end
 end
 
 --- file is absolut path
 local function py_run(file)
-  local py_env = venv()
-  if not py_env then
+  local py_venv = venv()
+  if not py_venv then
     Notify({
       "need to create python Virtual Environment first:",
-      '  `python3.xx -m venv .venv`',
+      '  `python3.xx -m venv .venv` or `uv venv`',
       '  `source .venv/bin/activate`',
     })
     return
   end
 
+  local py_path = vim.fs.joinpath(py_venv, '.venv/bin')
+
   local t = require('utils.my_term.instances').console
-  t.cmd = "source " .. py_env .. " && python3 --version && python3 " .. file
+  -- t.cmd = "which python3 && python3 --version && python3 " .. file  -- DEBUG
+  t.cmd = "python3 " .. file
+  t.env = { PATH = py_path..":$PATH" }  -- same as 'source .venv/bin/activate'
   t:stop()
   t:run()
 end
