372510e126a484ed0fdc938a5c2fcde582731f84
crazytaxi <crazytaxi.git@outlook.com>
2025-04-19 07:56:48 +1000

    upgrade: 19-04-2025

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -342,7 +342,7 @@ local plugins = {
   {
     "lewis6991/gitsigns.nvim",
     -- tag = "v1.0.2",
-    commit = "d600d39",
+    commit = "02eafb1",
     config = function() require("plugins.settings.git_signs") end,
 
     --- `nvim dir` 启动时直接打开 dir 时可能会造成 gitsigns 报错.
