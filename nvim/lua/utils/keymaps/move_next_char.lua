0d55142f2dd5ec5ec5900f6b2efd85b16553af3d
crazytaxi <crazytaxi.git@outlook.com>
2025-05-04 14:37:09 +1000

    chore

--- nvim/lua/utils/keymaps/move_next_char.lua
+++ nvim/lua/utils/keymaps/move_next_char.lua
@@ -5,7 +5,7 @@ M.move_next_char = function(m)
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
   local next_char = line:sub(col + 1, col + 1)
 
-  if next_char == ')' or next_char == ']' or next_char == '}' then
+  if next_char == ')' or next_char == ']' or next_char == '}' or next_char == '>' then
     --- x: cut, h: left, p: paste
     --- args could be: $: end of line, e: end of word, E: end of WORD
     vim.cmd.normal({args={"xh".. m .."p"}, bang=true})
