e1388588e8a61a4b20871088c61b06b40f77da2a
crazytaxi <crazytaxi.git@outlook.com>
2025-11-23 00:25:15 +1100

    update(swift): add swift_run(), swift_test() function

--- nvim/after/ftplugin/swift.lua
+++ nvim/after/ftplugin/swift.lua
@@ -1,6 +1,6 @@
-local function swift_run(file)
+local function swift_run_file()
   --- 先相对 HOME, 再相对 cwd. (absolut filepath)
-  file = vim.fn.fnamemodify(file, ':~:.')
+  local file = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':~:.')
 
   local t = require('utils.my_term.instances').console
   t.cmd = "swift -- " .. file
@@ -8,9 +8,60 @@ local function swift_run(file)
   t:run()
 end
 
+local function swift_run_proj()
+  local t = require('utils.my_term.instances').console
+  t.cmd = "swift run"
+  t:stop()
+  t:run()
+end
+
+local function swift_test_all()
+  local t = require('utils.my_term.instances').console
+  t.cmd = "swift test"
+  t:stop()
+  t:run()
+end
+
+local function swift_test_pkg()
+  --- 是否在 Tests 文件夹中
+  local dir = vim.fs.find({'Tests'}, {
+    upward = true,
+    stop = vim.uv.cwd(),
+    type = 'directory',
+  })
+
+  if #dir < 1 then
+    Notify('file is not in "Tests" dir', "ERROR")
+    return
+  end
+
+
+  --- 判断 class 是否为 class .*: XCTestCase
+  local lcontent = vim.fn.getline('.')  -- 获取行内容
+  local test_class = lcontent:match("class%s+([%w_]+)%s*:%s*XCTestCase")
+  if not test_class then
+    Notify('not XCTestCase class', "ERROR")
+    return
+  end
+
+  local t = require('utils.my_term.instances').console
+  t.cmd = "swift test --filter " .. test_class
+  t:stop()
+  t:run()
+end
+
 --- key mapping ------------------------------------------------------------------------------------
 --- run current_file ---
-vim.keymap.set('n', '<F5>', function() swift_run(vim.api.nvim_buf_get_name(0)) end, {
-  buffer = 0,
-  desc = "Fn 5: code: Run File",
-})
+local opt = { buffer = 0 }
+
+local swift_keymaps = {
+  {'n', '<F17>',  function() swift_run_file() end, opt, "Fn 5: code: Swift Run File"},
+  {'n', '<F5>',  function() swift_run_proj() end, opt, "Fn 5: code: Swift Run Project"},
+  {'n', '<F6>', function() swift_test_pkg() end, opt, "Fn 6: code: Swift Test Package"},
+  {'n', '<D-F6>', function() swift_test_all() end, opt, "Fn 6: code: Swift Test All"},
+}
+
+require('utils.keymaps').set(swift_keymaps)
+
+
+
