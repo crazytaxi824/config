b3e8036566ffc1a033693c0b3ab8ae33ee35322b
crazytaxi <crazytaxi.git@outlook.com>
2025-11-22 23:27:42 +1100

    update(lsp): switch to XCode sourcekit-lsp

---
+++ nvim/lua/lsp/plugins/lsp_config/langs/sourcekit.lua
@@ -0,0 +1,23 @@
+return {
+  --- `swift package init --type executable`, 会创建 Package.swift, Sources/, Tests/ 文件
+  root_dir = function(bufnr, on_dir)
+    local root = vim.fs.root(bufnr, 'Package.swift')
+    if root then
+      on_dir(root)
+      return
+    end
+
+    Notify(
+      {"'Package.swift' NOT found"},
+      "WARN",
+      {title={"LSP", "sourcekit.lua"}, timeout = false}
+    )
+  end,
+
+  --- sudo xcode-select -s /Applications/Xcode.app/Contents/Developer  (XCode)
+  --- sudo xcode-select -s /Library/Developer/CommandLineTools  (CommandLineTools)
+  --- 使用 XCode.app 中的 sourcekit-lsp 时需要设置环境变量, 如果使用 CommandLineTools 可以直接注释掉.
+  cmd_env = {
+    DEVELOPER_DIR = "/Applications/Xcode.app/Contents/Developer"
+  },
+}
