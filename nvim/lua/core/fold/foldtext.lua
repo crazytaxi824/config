2e424684f3de5967fddcd64a52c68d363b6e37a9
crazytaxi <crazytaxi.git@outlook.com>
2026-02-15 13:05:23 +1100

    docs

--- nvim/lua/core/fold/foldtext.lua
+++ nvim/lua/core/fold/foldtext.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+--- NOTE: vim.treesitter.foldexpr() 中 vim.v.foldend 是函数倒数第一行的 line nunmber
 function M.foldtext()
   --- startline: VVI: replace '\t' with 'N-spaces'. 否则 \t 会被认为是一个 char, 导致 line 开头的内容部分被隐藏.
   --- N-spaces 根据 buffer 的 tabstop 决定.
@@ -8,13 +9,13 @@ function M.foldtext()
   --- endline: trim foldend line
   local fe = vim.trim(vim.fn.getline(vim.v.foldend))
 
-  --- 这里主要是使用 setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr() 功能.
   if vim.wo.foldmethod == 'expr' then
     return fs .. ' ' .. Nerd_icons.ellipsis .. ' ' .. fe
   end
   return fs .. ' ' .. fe
 end
 
+--- NOTE: vim.lsp.foldexpr() 中 vim.v.foldend 是函数倒数第二行的 line nunmber, 所以 foldtext 需要不同设置.
 function M.foldtext_lsp()
   --- VVI: replace '\t' with 'N-spaces'. 否则 \t 会被认为是一个 char, 导致 line 开头的内容部分被隐藏.
   --- N-spaces 根据 buffer 的 tabstop 决定.
