88d165d1e8ee5556fdfffada61b90b6a7205fca6
crazytaxi <crazytaxi.git@outlook.com>
2026-02-16 21:50:10 +1100

    update(golangci): for version 2.x

--- nvim/lua/lsp/plugins/null_ls/tools/golangci_lint.lua
+++ nvim/lua/lsp/plugins/null_ls/tools/golangci_lint.lua
@@ -4,28 +4,12 @@
 return {
   --command = "path/to/golangci-lint",
 
-  --- VVI: 执行 golangci-lint 的 pwd. 默认是 params.root 即: null_ls.setup() 中的 root_dir / $ROOT
-  --- params 回调参数 ---------------------------------------------------------- {{{
-  --- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/MAIN.md#generators
-  --    content,    -- current buffer content (table, split at newline)
-  --    lsp_method, -- lsp method that triggered request (string)
-  --    method,  -- null-ls method that triggered generator (string)
-  --    row,     -- cursor's current row (number, zero-indexed)
-  --    col,     -- cursor's current column (number)
-  --    bufnr,   -- current buffer's number (number)
-  --    bufname, -- current buffer's full path (string)
-  --    ft,   -- current buffer's filetype (string)
-  --    root, -- current buffer's root directory (string)
-  -- -- }}}
-  -- cwd = function(params)
-  --   --- current buffer's dir, 相当于下面的 $DIRNAME.
-  --   return vim.fs.dirname(params.bufname)
-  -- end,
-
   ---  可以通过设置 setup() 中的 debug = true, 打开 `:NullLsLog` 查看命令行默认参数.
   args = function(params)
     local golangci_args = {
-      "run", "--fix=false", "--fast",
+      "run", "--fast-only", "--fix=false",
+      "--output.json.path=stdout",
+      "--show-stats=false",  -- 不显示最后的 (N) issues.
 
       --- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/HELPERS.md#args
       --- NOTE: 不能使用 $FILENAME lint 单个文件. 会导致其他 package 中定义的 var 无法被 golangci 找到.
@@ -36,16 +20,7 @@ return {
       --- VVI: 默认情况下运行 golangci-lint run 时 output 中 filename 是一个相对 pwd/cwd 的相对文件路径.
       --- null-ls 中已经处理了 cwd 和 golangci-lint output 中 filename 的 filepath 拼接, 所以这里不要自己设置 --path-prefix.
       -- "--path-prefix", "$DIRNAME",
-
-      "--print-issued-lines=false",
-      "--out-format=json",
-      "--issues-exit-code=1",
     }
-
-    --- DEBUG: 用
-    if __Debug_Neovim.null_ls then
-      Notify('golangci-lint ' .. table.concat(golangci_args, " "), "DEBUG", {title="Null-ls"})
-    end
 
     return golangci_args
   end,
