19cd193ed94744c354b9cf73f15cc8810f8cf18e
crazytaxi <crazytaxi.git@outlook.com>
2026-02-20 21:37:15 +1100

    refactor(lsp_config)!:

--- nvim/lua/lsp/lsp_config/update_config.lua
+++ nvim/lua/lsp/lsp_config/update_config.lua
@@ -7,46 +7,26 @@ local lsp_servers_map = require('lsp.svr_list').list
 --- cache local lsp settings
 local local_lsp_settings = nil
 
---- 加载 lsp 配置文件, "~/.config/nvim/lua/lsp/lsp_config/tools/..."
---- 如果文件存在, 则加载自定义设置, 如果没有自定义设置则加载默认设置.
-local function lsp_global_opts(lsp_tool, config)
-  local status_ok, global_config = pcall(require, "lsp.lsp_config.tools." .. lsp_tool)
+local function load_lsp_configs(lsp_tool)
+  --- 加载 lsp 配置文件, "~/.config/nvim/lua/lsp/lsp_config/tools/..."
+  --- 如果文件存在, 则加载自定义设置, 如果没有自定义设置则加载默认设置.
+  local status_ok, lsp_config = pcall(require, "lsp.lsp_config.tools." .. lsp_tool)
   if not status_ok then
-    return config
-  end
-  global_config = vim.deepcopy(global_config) -- VVI: deep copy 防止 require() 的内容后续被修改
-  return vim.tbl_deep_extend("force", config, global_config)
-end
-
---- 加载项目本地设置, 覆盖 global settings.
-local function lsp_local_opts(lsp_tool, config)
-  if not local_lsp_settings then
-    return config
+    lsp_config = {}
   end
 
-  local local_settings = local_lsp_settings[lsp_tool]
-  if not local_settings then
-    return config
+  --- 加载项目本地设置, 覆盖 global settings.
+  local local_settings = nil
+  if local_lsp_settings and local_lsp_settings[lsp_tool] then
+    local_settings = local_lsp_settings[lsp_tool]
   end
 
-  --- 如果原本没有设置 settings, 则设置 settings.
-  if not config.settings then
-    config.settings = local_settings
-    return config
+  --- merge settings
+  if local_settings then
+    return vim.tbl_deep_extend('force', lsp_config, {settings = local_settings})
   end
 
-  --- 如果原本设置了 settings, 则修改 settings 设置.
-  for key, value in pairs(local_settings) do
-    local cfg_settings = config.settings[key]
-    if cfg_settings then
-      --- key 存在
-      config.settings[key] = vim.tbl_deep_extend('force', cfg_settings, value)
-    else
-      --- key 不存在
-      config.settings[key] = value
-    end
-  end
-  return config
+  return lsp_config
 end
 
 local M = {}
@@ -64,13 +44,8 @@ end
 --- @param lsp_tool string
 M.lspconfig_setup = function(lsp_tool)
   --- config 必须包含 on_attach, capabilities 两个属性.
-  local config = require("lsp.lsp_config.client_config")
-  config = vim.deepcopy(config)  -- VVI: deep copy 防止 require() 的内容后续被修改
-
-  --- 先加载 global_opts
-  config = lsp_global_opts(lsp_tool, config)
-  config = lsp_local_opts(lsp_tool, config)
-
+  local common_config = require("lsp.lsp_config.client_config")
+  local config = vim.tbl_deep_extend('force', common_config, load_lsp_configs(lsp_tool))
   vim.lsp.config(lsp_tool, config)
 end
 
