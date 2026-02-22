7a0c686e1d025f9ca9e5d814a96f04c96968828a
crazytaxi <crazytaxi.git@outlook.com>
2026-02-22 21:13:13 +1100

    chore

--- nvim/lua/utils/my_term/deps/global.lua
+++ nvim/lua/utils/my_term/deps/global.lua
@@ -9,8 +9,8 @@ M.win_height = math.ceil(vim.o.lines/4)
 M.global_my_term_cache = {}
 
 --- 判断 terminal bufnr 是否存在, 是否有效
----@param bufnr integer
----@return boolean
+---@param bufnr? integer
+---@return boolean?
 function M.term_buf_exist(bufnr)
   return bufnr and vim.api.nvim_buf_is_valid(bufnr)
 end
