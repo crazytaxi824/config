5c69834acba13d73182ba3fa2180bd17e2a63582
crazytaxi <crazytaxi.git@outlook.com>
2026-02-24 20:38:06 +1100

    refactor(fzf_selected)

--- nvim/lua/core/funcs/fzf_edit.lua
+++ nvim/lua/core/funcs/fzf_edit.lua
@@ -3,16 +3,15 @@
 --- 如果是 fd 选择的结果, 则临时文件中记录的是 filepath/dir.
 --- 如果是 rg 返回的结果, 则临时文件中记录的是 <filepath:line:col:content>
 --- 结论: {+f} 临时文件中记录的是 fzf 中显示的结果.
----
---- @param fzf_tmp_file string filepath
-function FZF_selected(fzf_tmp_file)
-  if not vim.uv.fs_stat(fzf_tmp_file) then
-    Notify({"fzf `{+f}` tmp file is NOT readable.", "path: `" .. fzf_tmp_file .. "`"}, "ERROR")
-    return
-  end
 
+
+--- 获取 fzf 文件中 selected items
+---
+--- @param fzf_file string filepath
+--- @return vim.quickfix.entry[]|nil
+local function parse_fzf_items(fzf_file)
   --- 获取文件内容, readfile() 会按照 '\n' 返回一个 list.
-  local fzf_selected_items = vim.fn.readfile(fzf_tmp_file)
+  local fzf_selected_items = vim.fn.readfile(fzf_file)
 
   local fp_qf_list = {}
   for _, fzf_item in ipairs(fzf_selected_items) do
@@ -35,17 +34,33 @@ function FZF_selected(fzf_tmp_file)
   --- 一般不会出现传入 0 个 item 的情况.
   --- 就算在 fzf 中没有使用 <tab> 选择任何一个 item, fzf 会将当前 cursor 指向的 item 作为选中的 item.
   --- 所以一般情况下至少会有一个 item.
-  if #fp_qf_list == 0 then
-    Notify("fzf selected items is 0, please check fzf result.", "ERROR")
+  if #fp_qf_list > 0 then
+    return fp_qf_list
+  end
+
+  Notify("fzf selected items is 0, please check fzf result.", "ERROR")
+end
+
+--- 配合 fzf 用于 edit selected items
+---
+--- @param fzf_file string filepath
+function FZF_selected(fzf_file)
+  if not vim.uv.fs_stat(fzf_file) then
+    Notify({"fzf `{+f}` tmp file is NOT readable.", "path: `" .. fzf_file .. "`"}, "ERROR")
     return
   end
 
-  --- edit file
+  local fp_qf_list = parse_fzf_items(fzf_file)
+  if not fp_qf_list then
+    return
+  end
+
+  --- 直接打开第一个 file
   vim.cmd.edit(fp_qf_list[1].filename)
   local win_id = vim.api.nvim_get_current_win()
   vim.api.nvim_win_set_cursor(win_id, {fp_qf_list[1].lnum, fp_qf_list[1].col-1})  -- {1,0}-indexed
 
-  --- 如果有多个 item, 则放入 quickfix list.
+  --- 如果有多个 file, 则放入 quickfix list.
   if #fp_qf_list > 1 then
     --- put all items in quickfix_list.
     vim.fn.setqflist(fp_qf_list, 'r')  -- 'a' - append to quickfix_list; 'r' - replace quickfix_list with new items.
