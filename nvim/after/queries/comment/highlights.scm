bf50c7eb2a079855b18fa2d13b4bf7021389c015
crazytaxi <crazytaxi.git@outlook.com>
2026-02-25 13:03:04 +1100

    update(treesitter): highlight

--- nvim/after/queries/comment/highlights.scm
+++ nvim/after/queries/comment/highlights.scm
@@ -1,13 +1,15 @@
 ; extends
 ;;; `:help treesitter-predicates`
 
-; 将 VVI 加入到 warning
+; VVI: @comment.warning, (|xxx|) @constant, ":" @punctuation.delimiter.comment
+; VVI(message): comment
 ((tag
   (name) @comment.warning @nospell
-  ("(" @punctuation.bracket (user) @constant ")" @punctuation.bracket)?
-  ":" @punctuation.delimiter)
+  ("(" @punctuation.bracket.comment (user) @constant ")" @punctuation.bracket.comment)?
+  ":" @punctuation.delimiter.comment)
   (#any-of? @comment.warning "VVI" "DEPRECATED"))
 
+; VVI 加入到 @comment.warning
 ("text" @comment.warning @nospell
  (#any-of? @comment.warning "VVI" "DEPRECATED"))
 
