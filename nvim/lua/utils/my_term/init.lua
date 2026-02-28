2fef5ba1dd09a9af54c2ff6ba92e7282dfc8df8b
crazytaxi <crazytaxi.git@outlook.com>
2026-02-28 19:02:30 +1100

    refactor(my_term)

--- nvim/lua/utils/my_term/init.lua
+++ nvim/lua/utils/my_term/init.lua
@@ -3,6 +3,31 @@ local t_win = require('utils.my_term.deps.term_win')
 local t_act = require('utils.my_term.term_actions')
 local my_term = require("utils.my_term.my_term")
 
+
+--- source Python Virtual Environment
+---
+--- @param term_id integer
+local function python_env(term_id)
+  local py_venv = vim.fs.find({ '.venv/bin/activate' }, {
+    upward = true,
+    stop = vim.env.HOME,
+    type = "file",
+  })
+  if #py_venv < 1 then
+    return
+  end
+
+  --- 返回 MyTermPost.job_id
+  local job_id = g.get_job_id(term_id)
+  if not job_id then
+    error("no job_id after run()")
+  end
+
+  -- vim.fn.chansend(job_id, 'source ' .. py_venv[1] .. ' && clear\n')
+  vim.api.nvim_chan_send(job_id, 'source ' .. py_venv[1] .. ' && clear\n')
+end
+
+
 local M = {}
 
 --- new MyTerm object
@@ -27,55 +52,40 @@ function M.open_shell_term()
   end
 
   local t = M.get_term_by_id(vim.v.count1)
-  --- terminal 没有被缓存则 M.new()
-  if not t then
-    t = M.new({
-      id = vim.v.count1,
-      cmd = vim.go.shell,  -- `:help 'shell'`, 相当于 os.getenv('SHELL'), vim.env.SHELL
-      after_run = function(_, term_bufnr)
-        --- after_run 的时候 cursor 在 terminal window 中则执行 stopinsert.
-        if vim.api.nvim_win_get_buf(vim.api.nvim_get_current_win()) == term_bufnr then
-          vim.cmd.startinsert()
-        end
-      end,
-      on_exit = function(_, term_bufnr)
-        --- VVI: 手动 :bw 删除 buffer 时会触发 TermClose, 导致重复 wipeout buffer 而报错.
-        if vim.api.nvim_buf_is_valid(term_bufnr) then
-          vim.api.nvim_buf_delete(term_bufnr, {force=true})
-        end
-
-        --- NOTE: jobdone 的时候 cursor 在 terminal window 中则执行 stopinsert.
-        -- if vim.api.nvim_win_get_buf(vim.api.nvim_get_current_win()) == term_bufnr then
-        --   vim.cmd.stopinsert()
-        -- end
-      end,
-    })
-    t:run()
-
-    --- source Python Virtual Environment
-    local py_venv = vim.fs.find({'.venv/bin/activate'}, {
-      upward = true,
-      stop = vim.env.HOME,
-      type = "file",
-    })
-    if #py_venv < 1 then
-      return
+  if t then
+    --- open & enter window
+    if not t_act.open_win(t.id) then
+      error('cached my_term has No bufnr')
     end
-
-    --- t 是 MyTerm, 这里返回的是 MyTermPost.job_id
-    local job_id = g.get_job_id(t.id)
-    if not job_id then
-      error("no job_id after run()")
-    end
-
-    -- vim.fn.chansend(job_id, 'source ' .. py_venv[1] .. ' && clear\n')
-    vim.api.nvim_chan_send(job_id, 'source ' .. py_venv[1] .. ' && clear\n')
     return
   end
 
-  if not t_act.open_win(t.id) then
-    error('cached my_term has No bufnr')
-  end
+  --- terminal 没有被缓存则 M.new()
+  t = M.new({
+    id = vim.v.count1,
+    cmd = vim.go.shell,  -- `:help 'shell'`, 相当于 os.getenv('SHELL'), vim.env.SHELL
+    after_run = function(_, term_bufnr)
+      --- after_run 的时候 cursor 在 terminal window 中则执行 stopinsert.
+      if vim.api.nvim_win_get_buf(vim.api.nvim_get_current_win()) == term_bufnr then
+        vim.cmd.startinsert()
+      end
+    end,
+    on_exit = function(_, term_bufnr)
+      --- VVI: 手动 :bw 删除 buffer 时会触发 TermClose, 导致重复 wipeout buffer 而报错.
+      if vim.api.nvim_buf_is_valid(term_bufnr) then
+        vim.api.nvim_buf_delete(term_bufnr, {force=true})
+      end
+
+      --- NOTE: jobdone 的时候 cursor 在 terminal window 中则执行 stopinsert.
+      -- if vim.api.nvim_win_get_buf(vim.api.nvim_get_current_win()) == term_bufnr then
+      --   vim.cmd.stopinsert()
+      -- end
+    end,
+  })
+  t:run()
+
+  --- source Python Virtual Environment
+  python_env(t.id)
 end
 
 --- return an term object by id
