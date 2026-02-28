d90fd3f2d4c7972559ff919718b55c588ad86502
crazytaxi <crazytaxi.git@outlook.com>
2026-02-28 18:38:15 +1100

    refactor(my_term): instance

--- nvim/lua/utils/my_term/instances.lua
+++ nvim/lua/utils/my_term/instances.lua
@@ -1,11 +1,15 @@
+local g = require('utils.my_term.deps.global')
 local mt = require('utils.my_term')
 local fp = require('utils.filepath')
+
+local console_id = 1001
+
 
 local M = {}
 
 --- execute terminals: run cmd ---------------------------------------------------------------------
 M.console = mt.new({
-  id = 1001,
+  id = console_id,
   auto_scroll = true,
   console_output = true,  -- 这里使用 console_exec()
   after_run = function(_, term_bufnr)
@@ -19,7 +23,12 @@ M.console = mt.new({
 --- keymaps ----------------------------------------------------------------------------------------
 local opt = { silent = true }
 local my_term_keymaps = {
-  {'n', '<D-F5>', function() M.console:run() end, opt, "Fn 5: code: Re-Run Last cmd"},
+  {'n', '<D-F5>', function()
+    local tp = g.get_TermPost(console_id)
+    if tp then
+      tp:run()
+    end
+  end, opt, "Fn 5: code: Re-Run Last cmd"},
 }
 require('utils.keymaps').set(my_term_keymaps)
 
