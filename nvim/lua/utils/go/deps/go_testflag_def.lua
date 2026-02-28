0a1e70a0d24723a487b8f85095eeb7af13a5ec12
crazytaxi <crazytaxi.git@outlook.com>
2026-02-28 17:33:54 +1100

    chore

--- nvim/lua/utils/go/deps/go_testflag_def.lua
+++ nvim/lua/utils/go/deps/go_testflag_def.lua
@@ -9,6 +9,7 @@
 --- @field term_opts fun(opts: GoTestOpts): MyTermOpts
 
 
+
 --- @class GoTestFlagDict
 ---
 --- 可选 flag 排序, elem 必须为 `GoTestFlagDict.flags` 中的 keys
@@ -16,3 +17,24 @@
 ---
 --- 所有的 GoTestFlag
 --- @field flags table<string, GoTestFlag>
+
+
+
+--- @class GoTestOpts
+---
+--- 函数名 `^Test.*`, `^Benchmark.*` ...
+--- @field testfn_name string
+---
+--- go test -run | -bench | -fuzz
+--- @field mode 'run' | 'bench' | 'fuzz'
+---
+--- 'none' | 'cpu' | 'mem' | ...
+--- @field flag string
+---
+--- `go list -json`
+--- @field go_list table
+---
+--- 标记是否运行整个 project 所有 test
+--- @field project? string|boolean
+
+
