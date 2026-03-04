1cb54020ea2d1162e11f02388d338e09c4727b66
crazytaxi <crazytaxi.git@outlook.com>
2026-03-05 10:04:23 +1100

    docs

--- nvim/lua/utils/keymaps/close_other_buf.lua
+++ nvim/lua/utils/keymaps/close_other_buf.lua
@@ -7,7 +7,7 @@ M.delete_all_other_buffers = function()
 
   --- NOTE: nvimtree, tagbar, terminal 不会被关闭, 因为他们是 unlisted.
   for _, bufinfo in ipairs(vim.fn.getbufinfo({ buflisted = 1 })) do  -- 获取 listed buffer
-    if bufinfo.changed == 0    -- 没有修改后未保存的内容.
+    if bufinfo.changed == 0    -- 'vim.bo.modified' 没有修改后未保存的内容.
       and bufinfo.hidden == 1  -- 是隐藏状态的 buffer. 如果是 active 状态(即: 正在显示的 buffer, 例如当前 buffer), 不会被删除.
     then
       table.insert(buf_list, bufinfo.bufnr)
