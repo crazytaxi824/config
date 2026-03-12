42ff264a21cc308ec788866b7b2221ebef966212
crazytaxi <crazytaxi.git@outlook.com>
2026-03-12 18:51:10 +1100

    update(statusline): add theme

---
+++ nvim/lua/utils/statusline/theme.lua
@@ -0,0 +1,13 @@
+--- @class StlTheme
+--- @field default vim.api.keyset.highlight
+--- @field insert vim.api.keyset.highlight
+--- @field visual vim.api.keyset.highlight
+--- @field replace vim.api.keyset.highlight
+--- @field command vim.api.keyset.highlight
+
+
+--- @type StlTheme
+local theme = {
+  default = {},
+}
+
