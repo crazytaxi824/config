87af77fc49188c9fe2ff2dd9f52b08317390cd6b
crazytaxi <crazytaxi.git@outlook.com>
2026-04-24 13:58:16 +1000

    fix(antidote): update

--- zsh_config/plugins.zsh
+++ zsh_config/plugins.zsh
@@ -8,15 +8,20 @@
 	local zsh_plugins_txt="$XDG_CONFIG_HOME/antidote/zsh_plugins.txt"   # antidote 配置文件
 	local zsh_plugins_static="$HOME/.antidote_plugins.zsh"   # antidote 生成的静态文件
 
-	if [[ -f "$ANTIDOTE_DIR/antidote.zsh" ]]; then
+	if [[ -f "$ANTIDOTE_DIR/functions/antidote" ]]; then
+		# Lazy-load antidote from its functions directory.
+		# NOTE: 这里使用 autoload 而没用 source $ANTIDOTE_DIR/antidote.zsh, 因为外部需要
+		# `antidote update` 命令来更新 plugins, 但又不用每次都 source zsh 文件, 节约性能.
+		fpath=($ANTIDOTE_DIR/functions $fpath)
+		autoload -Uz antidote
+		# source "$ANTIDOTE_DIR/antidote.zsh"
+
 		# 确保配置文件存在
 		[[ -f "$zsh_plugins_txt" ]] || touch "$zsh_plugins_txt"
 
+		# 生成静态 zsh 文件
+		# "$zsh_plugins_txt" -nt "$zsh_plugins_static" A 比 B 更新
 		if [[ ! -f "$zsh_plugins_static" || "$zsh_plugins_txt" -nt "$zsh_plugins_static" ]]; then
-			# 加载 antidote 命令
-            source "$ANTIDOTE_DIR/antidote.zsh"
-
-			# 生成静态 zsh 文件
             antidote bundle < "$zsh_plugins_txt" > "$zsh_plugins_static"
 		fi
 
