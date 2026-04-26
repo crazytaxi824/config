667bcfab8b1079ae3b91576c85f915994f5de254
crazytaxi <crazytaxi.git@outlook.com>
2026-04-27 00:07:12 +1000

    update(zshrc): add ssh-agent function

--- zsh_config/funcs.zsh
+++ zsh_config/funcs.zsh
@@ -4,6 +4,17 @@ function rm() {
 	return 2   # exit code
 }
 
+# macos 会自动启动 ssh-agent, 如果手动再次启动会导致创建 `~/.ssh/agent/xxx` socket 文件.
+function ssh-agent() {
+	if [ -S "$SSH_AUTH_SOCK" ]; then
+		echo '\e[33m"ssh-agent" is running, use "ssh-add" directly\e[0m'
+		return 2   # exit code
+	else
+		# command 关键字跳过函数，直接调用二进制
+		command ssh-agent "$@"
+	fi
+}
+
 # 加载自定义 zsh 函数 ------------------------------------------------------------------------------
 fpath=(~/.config/zsh_config/funcs $fpath)
 autoload -Uz 256color
