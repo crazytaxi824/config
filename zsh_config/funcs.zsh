d0cf91645188965ed0cb54d57f6067ca6d5f40b5
crazytaxi <crazytaxi.git@outlook.com>
2026-04-27 20:03:55 +1000

    chore(zshrc)

--- zsh_config/funcs.zsh
+++ zsh_config/funcs.zsh
@@ -16,7 +16,7 @@ function ssh-agent() {
 }
 
 # 加载自定义 zsh 函数 ------------------------------------------------------------------------------
-fpath=(~/.config/zsh_config/funcs $fpath)
+fpath=($HOME/.config/zsh_config/funcs $fpath)
 autoload -Uz 256color
 autoload -Uz backup_config
 autoload -Uz e
