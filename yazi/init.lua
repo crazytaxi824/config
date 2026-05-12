acff716e016a81537b51e77128c80a87105844d9
crazytaxi <crazytaxi.git@outlook.com>
2026-05-12 10:56:30 +1000

    update(yazi): config

--- yazi/init.lua
+++ yazi/init.lua
@@ -19,16 +19,14 @@ end, 3300, Status.LEFT)
 -- Show `mime` in status bar
 Status:children_add(function()
   local h = cx.active.current.hovered
-  if h and not h.cha.is_dir then
-    local handle = io.popen("file -b --mime-type " .. shellescape(tostring(h.url.path)))
+  if h then
+    local handle = io.popen("file -b --mime-type " .. shellescape(tostring(h.url)))
     local result = handle:read("*l")
     handle:close()
 
     return ui.Line(
       ui.Span(string.format("[%s] ", result)):fg("blue")
     )
-  else
-    return ""
   end
 end, 100, Status.RIGHT)
 
