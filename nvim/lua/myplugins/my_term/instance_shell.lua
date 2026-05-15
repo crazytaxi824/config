21662ec3a0fb8166c5753c8ac4670302bdbfe8a4
crazytaxi <crazytaxi.git@outlook.com>
2026-05-15 21:53:02 +1000

    fix(nvim/myterm): potential command race when load python_env in shell terminal

--- nvim/lua/myplugins/my_term/instance_shell.lua
+++ nvim/lua/myplugins/my_term/instance_shell.lua
@@ -47,12 +47,18 @@ function M.open_shell_term()
 
   --- terminal 没有被缓存则 M.new()
   local t = mt.new(vim.v.count1, {
-    after_run = function(_, term_bufnr)
+    after_run = function(term, term_bufnr)
       --- after_run 的时候 cursor 在 terminal window 中则执行 startinsert.
       if vim.api.nvim_win_get_buf(vim.api.nvim_get_current_win()) == term_bufnr then
         vim.cmd.startinsert()
       end
+
+      --- source Python Virtual Environment
+      vim.schedule(function()
+        python_env(term.id)
+      end)
     end,
+
     on_exit = function(_, term_bufnr)
       --- 在 jobdone (exit) 的时候 :bwipeout terminal buffer
       if vim.api.nvim_buf_is_valid(term_bufnr) then
@@ -60,10 +66,8 @@ function M.open_shell_term()
       end
     end,
   })
-  t:run(vim.go.shell)  -- `:help 'shell'`, 相当于 os.getenv('SHELL'), vim.env.SHELL
 
-  --- source Python Virtual Environment
-  python_env(t.id)
+  t:run(vim.go.shell)  -- `:help 'shell'`, 相当于 os.getenv('SHELL'), vim.env.SHELL
 end
 
 return M
