c6db2aebe4db407dc5722fb968195e4b9e240c53
crazytaxi <crazytaxi.git@outlook.com>
2026-05-20 12:05:17 +1000

    update(nvim/color)

---
+++ nvim/lua/core/palettes.lua
@@ -0,0 +1,26 @@
+local palette_1 = {
+  red    = { "#F89A8A", "#E8705F", "#D14D41", "#C03E35", "#AF3029", "#942822", "#6C2010" },
+  orange = { "#F9AE77", "#EC8B49", "#DA702C", "#CB6120", "#BC5215", "#9D4310", "#71320D" },
+  yellow = { "#ECCB60", "#DFB431", "#D0A215", "#BE9207", "#AD8301", "#8E6B01", "#664D01" },
+  green  = { "#BEC97E", "#A0AF54", "#879A39", "#768D21", "#66800B", "#536907", "#3D4C07" },
+  cyan   = { "#87D3C3", "#5ABDAC", "#3AA99F", "#2F968D", "#24837B", "#1C6C66", "#164F4A" },
+  blue   = { "#92BFDB", "#66A0C8", "#4385BE", "#3171B2", "#205EA6", "#1A4F8C", "#163B66" },
+  purple = { "#C4B9E0", "#A699D0", "#8B7EC8", "#735EB5", "#5E409D", "#4F3685", "#3C2A62" },
+  magenta= { "#F4A4C2", "#E47DA8", "#CE5D97", "#B74583", "#A02F6F", "#87285E", "#641F46" },
+}
+
+local palette_2 = {
+  red    = {  },
+  magenta= {  },
+  yellow = { "#fdf3d7", "#fce4af", "#fcd28d", "#f8bd6f", "#f9a04a" },
+  orange = { "#fdf3d7", "#ee8c3c", "#ed792f", "#ed6922", "#ed5615" },
+  cyan   = { "#bdfff6", "#90e6e9", "#5acfcf", "#2cc4bd", "#00B7af" },
+  teal   = { "#E1F7F8", "#C8EDEF", "#A0D2D6", "#7CA9B6", "#517888" },
+  teal2  = { "#E0FBFC", "#c2f5f9", "#93e8ef", "#5ad8e5", "#27c8d9" },
+  purple = { "#f8f0fd", "#f3dafa", "#eebefa", "#e499f6", "#da78f3" },
+  pink   = { "#fff0f6", "#ffdeeb", "#fcc2d7", "#faa2c1", "#f783ac" },
+
+  cyan_teal  = { "#cff4ed", "#b1e3db", "#93c7c5", "#6ca0a4", "#4b7e88" },
+  blue_teal  = { "#c6eaf3", "#8fd3ed", "#5aa9cd", "#357aa7", "#0e4775" },
+  blue_azure = { "#e5f5fc", "#cdecfe", "#a0d7fd", "#6ebff8", "#42abf4" },
+}
