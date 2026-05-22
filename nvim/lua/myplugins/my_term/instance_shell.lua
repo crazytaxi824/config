0b33b24e7c4bac31937054161fd06b6d75469503
crazytaxi <crazytaxi.git@outlook.com>
2026-05-22 17:08:55 +1000

    chore(myterm): remove vim.schedule() for shell

--- nvim/lua/myplugins/my_term/instance_shell.lua
+++ nvim/lua/myplugins/my_term/instance_shell.lua
@@ -48,15 +48,13 @@ function M.open_shell_term()
   --- terminal 没有被缓存则 M.new()
   local t = mt.new(vim.v.count1, {
     on_start = function(term, term_bufnr)
-      vim.schedule(function()
-        --- on_init 的时候 cursor 在 terminal window 中则执行 startinsert.
-        if vim.api.nvim_get_current_buf() == term_bufnr then
-          vim.cmd.startinsert()
-        end
+      --- on_init 的时候 cursor 在 terminal window 中则执行 startinsert.
+      if vim.api.nvim_get_current_buf() == term_bufnr then
+        vim.cmd.startinsert()
+      end
 
-        --- source Python Virtual Environment
-        python_env(term.id)
-      end)
+      --- source Python Virtual Environment
+      python_env(term.id)
     end,
 
     on_exit = function(_, term_bufnr)
