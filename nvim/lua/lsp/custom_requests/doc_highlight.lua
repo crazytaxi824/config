bfbb612bdabf82d9c350f1a8bf2234be29eb5ed6
crazytaxi <crazytaxi.git@outlook.com>
2026-05-24 23:43:30 +1000

    chore(nvim/doc_hl): check cursor inside prvious range according to lsp client offset_encoding

--- nvim/lua/lsp/custom_requests/doc_highlight.lua
+++ nvim/lua/lsp/custom_requests/doc_highlight.lua
@@ -24,28 +24,68 @@
 local ms = vim.lsp.protocol.Methods
 
 -- cache last documentHighlight references
----@type {bufnr: integer, refs: lsp.DocumentHighlight[]}
+---@type { bufnr: integer, groups: { refs: lsp.DocumentHighlight[], offset_encoding?: string }[] }
 local last_results = {}
 
 
 local M = {}
 
+---@param a_line integer
+---@param a_char integer
+---@param b_line integer
+---@param b_char integer
+local function pos_le(a_line, a_char, b_line, b_char)
+  return a_line < b_line or (a_line == b_line and a_char <= b_char)
+end
+
+---@param a_line integer
+---@param a_char integer
+---@param b_line integer
+---@param b_char integer
+local function pos_lt(a_line, a_char, b_line, b_char)
+  return a_line < b_line or (a_line == b_line and a_char <= b_char)
+end
+
 -- 判断当前 cursor 是否仍然在 range 之内
 --
+---@param bufnr integer
 ---@param refs lsp.DocumentHighlight[]
+---@param offset_encoding? string
 ---@return boolean|nil inside
-local function cursor_inside_range(refs)
-  local cur_pos = vim.fn.getcharpos('.')  -- 1-based character index
-  local cur_line, cur_col = cur_pos[2]-1, cur_pos[3]-1  -- change to 0-based index
+local function cursor_inside_range(bufnr, refs, offset_encoding)
+  local row, byte_col = unpack(vim.api.nvim_win_get_cursor(0))
+  local cur_line = row - 1
 
-  -- result.range: 0-based index
+  -- 将 cursor col 根据 lsp offset_encoding 进行转换
+  local text = vim.api.nvim_buf_get_lines(bufnr, cur_line, cur_line + 1, false)[1] or ''
+  local cur_char = vim.str_utfindex(text, offset_encoding or 'utf-16', byte_col, false)
+
   for _, ref in ipairs(refs) do
-    local start_line, start_char = ref['range']['start']['line'], ref['range']['start']['character']
-    local end_line, end_char = ref['range']['end']['line'], ref['range']['end']['character']
-    if cur_line >= start_line and cur_col >= start_char and cur_line <= end_line and cur_col <= end_char then
-      return true  -- cursor still inside references range
+    local range = ref.range
+    if range then
+      local s = range.start
+      local e = range["end"]
+
+      if pos_le(s.line, s.character, cur_line, cur_char)
+        and pos_lt(cur_line, cur_char, e.line, e.character)
+      then
+        return true
+      end
     end
   end
+
+  return false
+end
+
+-- cursor is in any of the lsp clients highlight range
+---@return boolean
+local function cursor_inside_buf_ranges(bufnr)
+  for _, group in ipairs(last_results.groups or {}) do
+    if cursor_inside_range(bufnr, group.refs, group.offset_encoding) then
+      return true
+    end
+  end
+  return false
 end
 
 -- 创建 vim.lsp.buf_request_all(_, _, params, _) 中的 params
@@ -61,11 +101,10 @@ local function client_positional_params(params)
   end
 end
 
--- 根据 https://github.com/neovim/neovim/blob/master/runtime/lua/vim/lsp/buf.lua 中 M.hover(config) 函数修改
+-- https://github.com/neovim/neovim/blob/master/runtime/lua/vim/lsp/buf.lua
+-- 根据 M.hover(config) 函数中 vim.lsp.buf_request_all(_, _, _, handler) 中的 handler 修改
 --
--- 创建 vim.lsp.buf_request_all(_, _, _, handler) 中的 handler
---
----@param results table<integer,{err: lsp.ResponseError?, result: any}>
+---@param results table<integer, { err: lsp.ResponseError?, result: lsp.DocumentHighlight[] }>
 ---@param ctx lsp.HandlerContext
 local function doc_highlight_handler(results, ctx)
   local bufnr = assert(ctx.bufnr)
@@ -74,8 +113,8 @@ local function doc_highlight_handler(results, ctx)
     return
   end
 
-  ---@type lsp.DocumentHighlight[]
-  local cache = {}
+  ---@type { refs: lsp.DocumentHighlight[], offset_encoding?: string }[]
+  local groups = {}
 
   for client_id, resp in pairs(results) do
     local err = resp.err
@@ -83,17 +122,25 @@ local function doc_highlight_handler(results, ctx)
     if err then
       vim.lsp.log.error(err.code, err.message)
     elseif refs then
-      cache = vim.list_extend(cache, refs)
       local client = assert(vim.lsp.get_client_by_id(client_id))
+
+      ---@type { refs: lsp.DocumentHighlight[], offset_encoding?: string }
+      local group = {}
+      group.refs = refs
+      group.offset_encoding = client.offset_encoding
+      table.insert(groups, group)
+
       vim.lsp.util.buf_highlight_references(bufnr, refs, client.offset_encoding)  -- NOTE: highlight references
     end
   end
 
-  -- VVI: cache result
-  last_results = { bufnr = bufnr, refs = cache }
+  --- 刷新 cache 内容
+  last_results = { bufnr=bufnr, groups=groups }
 end
 
-M.setup = function (client, bufnr)
+---@param client vim.lsp.Client
+---@param bufnr integer
+M.setup = function(client, bufnr)
   -- VVI: 这里必须使用 augroup, 否则在 `:LspRestart` 的情况下会叠加多个 autocmd.
   local group_id = vim.api.nvim_create_augroup("my_documentHighlight_#buf:"..bufnr, {clear=true})
 
@@ -103,15 +150,16 @@ M.setup = function (client, bufnr)
     group = group_id,
     buffer = bufnr,  -- 对指定 buffer 有效
     callback = function(args)
-      if last_results.bufnr and last_results.bufnr == bufnr
-        and last_results.refs and #last_results.refs > 0
-        and cursor_inside_range(last_results.refs)
-      then
+      if last_results.bufnr == bufnr and cursor_inside_buf_ranges(bufnr) then
         return
       end
 
       -- 清除旧 highlight
+      if last_results.bufnr then
+        vim.lsp.util.buf_clear_references(last_results.bufnr)
+      end
       vim.lsp.util.buf_clear_references(bufnr)
+      last_results = {}
 
       -- send 'textDocument/documentHighlight' request. 重新渲染 highlight
       vim.lsp.buf_request_all(bufnr, ms.textDocument_documentHighlight, client_positional_params(), doc_highlight_handler)
@@ -124,23 +172,33 @@ M.setup = function (client, bufnr)
     group = group_id,
     buffer = bufnr,  -- 对指定 buffer 有效
     callback = function(args)
-      if last_results.bufnr and last_results.bufnr == bufnr
-        and last_results.refs and #last_results.refs > 0
-        and not cursor_inside_range(last_results.refs)
-      then
+      if not last_results.bufnr then
+        return
+      end
+
+      if last_results.bufnr ~= bufnr then
+        vim.lsp.util.buf_clear_references(last_results.bufnr)
+        last_results = {}
+        return
+      end
+
+      if not cursor_inside_buf_ranges(bufnr) then
         vim.lsp.util.buf_clear_references(bufnr)
+        last_results = {}
+        return
       end
     end,
     desc = "LSP: clear highlight when cursor move out of word",
   })
 
   -- cursor 离开 window 时清除 references highlight
+  -- 主要是用于进入了没有 lsp 的 buffer
   vim.api.nvim_create_autocmd({'WinLeave'}, {
     group = group_id,
     buffer = bufnr,  -- 对指定 buffer 有效
     callback = function(args)
-      last_results = {}
       vim.lsp.util.buf_clear_references(bufnr)
+      last_results = {}
     end,
     desc = "LSP: clear highlight when leave window",
   })
